b5d8778270fd100a14182247f37461347cc65468
flavio@hurd <>
2008-05-29 20:02:38 +0000

    Temporarily set cffi libraries directory to include local helper libraries.

--- hurd.lisp
+++ hurd.lisp
@@ -3,7 +3,17 @@
 (asdf:oos 'asdf:load-op 'cffi)
 
 ; define used packages
-(load "packages.lisp")
+(load "packages")
+
+(defvar *helper-libraries-directory*
+  (truename "./hurd/helper-libs/")) ; FIXME
+
+(setf cffi:*foreign-library-directories*
+  (list #p"/lib/" *helper-libraries-directory*))
+
+; load some utilities
+(load "common/common")
 
 ; load package's implementation
-(load "mach/mach.lisp")
+(load "mach/mach")
+(load "hurd/hurd")
