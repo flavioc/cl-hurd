1d6ba035be22397104f42c9f91ef5637aa9fd9a0
flavio@hurd <>
2008-05-29 20:01:10 +0000

    Define hurd error and hurd common packages.

--- packages.lisp
+++ packages.lisp
@@ -1,11 +1,23 @@
 
 (in-package #:cl-user)
 
+(defpackage :cl-hurd.common
+  (:nicknames :hurd-common)
+  (:use :cl :cffi)
+  (:export :largest-representable-number
+		   :num-bits
+		   :define-helper-library))
+
+(defpackage :cl-hurd.error
+  (:nicknames :hurd-error)
+  (:use :cl :cffi :hurd-common))
+
 (defpackage :cl-mach
   (:nicknames :mach)
-  (:use :cl :cffi))
+  (:use :cl :cffi :hurd-common)
+  (:export :task-self))
 
 (defpackage :cl-hurd
   (:nicknames :hurd)
-  (:use :cl :cffi :mach))
+  (:use :cl :cffi :mach :hurd-common))
 
