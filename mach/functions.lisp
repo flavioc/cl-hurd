ab2b98c5a71b743de6263308c0930440d25c5642
flavio@localhost.localdomain <>
2008-06-03 18:35:25 +0100

    Port deallocate function and macro with-port.

--- mach/functions.lisp
+++ mach/functions.lisp
@@ -1,6 +1,6 @@
 
 ; mach_task_self
-(defcfun ("mach_task_self" %mach-task-self) port)
+(defcfun ("mach_task_self" %mach-task-self) ipc-space)
 
 (defun task-self ()
   "Returns a send right associated with the task_self port"
@@ -8,3 +8,17 @@
 
 (defun port-valid (p)
   (numberp p))
+
+(defcfun ("mach_port_deallocate" %mach-port-deallocate)
+		 kern-return
+		 (task port)
+		 (name port))
+
+(defun port-deallocate (name &optional (task (task-self)))
+  (%mach-port-deallocate task name))
+
+(defmacro with-port ((port-name creation &key (task 'self)) &body body)
+  `(let ((,port-name ,creation))
+	 (when (port-valid ,port-name)
+	   ,@body
+	   (port-deallocate ,port-name ,@(unless (eq task 'self) (list task))))))
