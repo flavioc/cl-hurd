45790702ca2768496a6364e6ca09f97591e3f7e9
flavio@localhost.localdomain <>
2008-06-03 18:34:09 +0100

    with-gensyms and string->integer utilities.

--- common/utils.lisp
+++ common/utils.lisp
@@ -8,6 +8,7 @@
   (* 8 bytes))
 
 (defmacro define-helper-library (name)
+  "Defines a new helper library using CFFI"
   (let* ((name-string (string-downcase (string name)))
 		 (library-name (concatenate 'string name-string ".so"))
 		 (full-name (intern library-name)))
@@ -15,3 +16,34 @@
 	   (define-foreign-library ,full-name
 	     (:unix ,library-name))
 	   (use-foreign-library ,full-name))))
+
+(defmacro with-gensyms ((&rest names) &body body)
+  "Use a list of generated symbols"
+  `(let ,(loop for n in names collect `(,n (gensym)))
+	 ,@body))
+
+(defun char->value (ch)
+  (case ch
+	(#\0 0)
+	(#\1 1)
+	(#\2 2)
+	(#\3 3)
+	(#\4 4)
+	(#\5 5)
+	(#\6 6)
+	(#\7 7)
+	(#\8 8)
+	(#\9 9)
+	(#\a 10)
+	(#\b 11)
+	(#\c 12)
+	(#\d 13)
+	(#\e 14)
+	(#\f 15)))
+
+(defun string->integer (str base)
+  (let ((e -1))
+	(reduce (lambda (current accum)
+			  (incf e)
+			  (+ accum (* (char->value current) (expt base e))))
+			str :from-end t :initial-value 0)))
