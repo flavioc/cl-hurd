a9c4fd8ef5978820bc1504b577c35623ff8eb964
flavio@localhost.localdomain <>
2008-06-03 16:11:02 +0100

    Rework error code translation mechanism.

--- hurd/error.lisp
+++ hurd/error.lisp
@@ -10,26 +10,91 @@
   (:actual-type :int)
   (:simple-parser err))
 
+;; bits/errno.h
+;;
 (defconstant +recognized-error-codes+
-			 '((perm 1)
-			   (noent 2)
-			   (srch 3))) ; FIXME: add more
+			 '((1 :not-permitted)
+			   (2 :no-such-file)
+			   (3 :no-such-process)
+			   (4 :interrupted-syscall)
+			   (5 :io-error)
+			   (6 :no-such-device-address)
+			   (7 :argument-list-too-long)
+			   (8 :exec-format-error)
+			   (9 :bad-fd)
+			   (10 :no-child-procs)
+			   (11 :deadlock-avoided)
+			   (12 :no-memory)
+			   (13 :permission-denied)
+			   (14 :bad-address)
+			   (15 :block-device-required)
+			   (16 :resource-busy)
+			   (17 :file-exists)
+			   (18 :invalid-cross-device-link)
+			   (19 :no-such-device)
+			   (20 :not-directory)
+			   (21 :is-a-directory)
+			   (22 :invalid-argument)
+			   (24 :too-many-open-files)
+			   (23 :too-many-open-files-system)
+			   (25 :inappropriate-ioctl-device)
+			   (26 :text-file-busy)
+			   (27 :file-too-large)
+			   (28 :no-space-left)
+			   (29 :illegal-seek)
+			   (30 :read-only-fs)
+			   (31 :too-many-links)
+			   (32 :broken-pipe)
+			   (33 :num-argument-out-domain)
+			   (34 :num-result-out-range)
+			   (35 :resource-unavailable)
+			   (36 :operation-in-progress)
+			   (37 :operation-already-in-progress)
+			   (38 :socket-op-on-nonsocket)
+			   (39 :message-too-long)
+			   (40 :protocol-wrong-type-socket)
+			   (41 :protocol-unavailable)
+			   (42 :protocol-not-supported)
+			   (43 :socket-type-not-supported)
+			   (44 :operation-not-supported)
+			   (45 :address-family-not-supported)
+			   (46 :adress-already-in-use)
+			   (47 :address-unavailable)
+			   (48 :network-down)
+			   (49 :network-unreachable)
+			   (50 :network-dropped-connection)
+			   (51 :connection-abort)
+			   (52 :connection-reset-by-peer)
+			   (53 :no-buffer-space)
+			   (54 :transport-endpoint-already-connected)
+			   )) ; FIXME: add more
 
-(defmacro translate-hurd-error-codes (var &key (direction :id-to-symbol))
-  `(case ,var
-	 ,@(if (eq direction :id-to-symbol)
-		 (list 0 t)
-		 (list t 0))
-	 ,@(loop for (name id) in +recognized-error-codes+
-			 collect (if (eq direction 'id-to-symbol)
-					   (list name (%get-hurd-error-code id))
-					   (list (%get-hurd-error-code id) name)))
-	 (t nil)))
+(define-condition unrecognized-error-code (error)
+  ((code :initarg :code :reader code)))
+
+(defmacro error-id-to-symbol (var)
+	`(case ,var
+	   ,@(loop for (id name) in +recognized-error-codes+
+			   collect (list (%get-hurd-error-code id) name))
+	   (0 t)
+	   (otherwise
+		 (warn "Identifier ~a not recognized" ,var)
+		 nil)))
+
+(defmacro error-symbol-to-id (var)
+  `(cond
+	 ((eq ,var t) 0)
+	 (t
+	   (case ,var
+		 ,@(loop for (id name) in +recognized-error-codes+
+				 collect (list name (%get-hurd-error-code id)))
+		 (otherwise 
+		   (error 'unrecognized-error-code :code ,var))))))
 
 (defmethod translate-from-foreign (value (type error-type))
   "Translates an error value to a symbol"
-  (get-hurd-error-codes value :direction :id-to-symbol))
+  (error-id-to-symbol value))
 
 (defmethod translate-to-foreign (value (type error-type))
   "Translates a lisp error code to a foreign one (ints)"
-  (translate-hurd-error-codes value :direction :symbol-to-id))
+  (error-symbol-to-id value))
