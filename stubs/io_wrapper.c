c17f5045fbe8ed6be24de6f1f5c308549a999365
flavio@localhost.localdomain <>
2008-06-05 22:46:49 +0100

    Add io_wrapper.

---
+++ stubs/io_wrapper.c
@@ -0,0 +1,297 @@
+
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE 1
+#endif
+
+#include <mach/boolean.h>
+#include <mach/kern_return.h>
+#include <mach/message.h>
+#include <mach/mig_errors.h>
+#include <mach/mig_support.h>
+
+#include <mach/std_types.h>
+#include <mach/mach_types.h>
+#include <device/device_types.h>
+#include <device/net_status.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/statfs.h>
+#include <sys/resource.h>
+#include <sys/utsname.h>
+#include <hurd/hurd_types.h>
+
+/* routines that can be set by the middle level */
+typedef enum {
+	IO_WRITE,
+	IO_READ,
+	IO_SEEK,
+	IO_READABLE,
+	IO_SET_ALL_OPENMODES,
+	IO_GET_OPENMODES,
+	IO_CLEAR_OPENMODES,
+	IO_ASYNC,
+	IO_MOD_OWNER,
+	IO_GET_OWNER,
+	IO_GET_ICKY_ASYNC_ID,
+	IO_SELECT,
+	IO_STAT,
+	IO_REAUTHENTICATE,
+	IO_RESTRICT_AUTH,
+	IO_DUPLICATE,
+	IO_SERVER_VERSION,
+	IO_MAP,
+	IO_MAP_CNTL,
+	IO_RELEASE_CONCH,
+	IO_EOFNOTIFY,
+	IO_PRENOTIFY,
+	IO_POSTNOTIFY,
+	IO_READSLEEP,
+	IO_SIGIO,
+	IO_PATHCONF,
+	IO_IDENTITY,
+	IO_REVOKE
+} IoRoutine;
+
+/* function wrappers follows... */
+
+kern_return_t
+lisp_io_write(io_t io_object,
+		data_t data,
+		mach_msg_type_number_t dataCnt,
+		loff_t offset,
+		vm_size_t *amount)
+{
+	return EOPNOTSUPP;
+}
+
+kern_return_t
+lisp_io_read(io_t io_object,
+		data_t *data,
+		mach_msg_type_number_t *dataCnt,
+		loff_t offset,
+		vm_size_t amount)
+{
+	return EOPNOTSUPP;
+}
+
+kern_return_t
+lisp_io_seek(io_t io_object,
+		loff_t offset,
+		int whence,
+		loff_t *newp)
+{
+	return EOPNOTSUPP;
+}
+
+kern_return_t
+lisp_io_readable(io_t io_object,
+		vm_size_t *amount)
+{
+	return EOPNOTSUPP;
+}
+
+kern_return_t
+lisp_io_set_all_openmodes(io_t io_object,
+		int newbits)
+{
+	return EOPNOTSUPP;
+}
+
+kern_return_t
+lisp_io_get_openmodes(io_t io_object,
+		int *bits)
+{
+	return EOPNOTSUPP;
+}
+
+kern_return_t
+lisp_io_set_some_openmodes(io_t io_object,
+		int bits_to_set)
+{
+	return EOPNOTSUPP;
+}
+
+kern_return_t
+lisp_io_clear_some_openmodes(io_t io_object,
+		int bits_to_clear)
+{
+	return EOPNOTSUPP;
+}
+
+kern_return_t
+lisp_io_async(io_t io_object,
+		mach_port_t notify_port,
+		mach_port_t *async_id_port,
+		mach_msg_type_name_t *async_id_portPoly)
+{
+	return EOPNOTSUPP;
+}
+
+
+kern_return_t
+lisp_io_mod_owner(io_t io_object,
+		pid_t owner)
+{
+	return EOPNOTSUPP;
+}
+
+kern_return_t
+lisp_io_get_owner(io_t io_object,
+		pid_t *owner)
+{
+	return EOPNOTSUPP;
+}
+
+kern_return_t
+lisp_io_get_icky_async_id(io_t io_object,
+		mach_port_t *icky_async_id_port,
+		mach_msg_type_name_t *icky_async_id_portPoly)
+{
+	return EOPNOTSUPP;
+}
+
+kern_return_t
+lisp_io_select(io_t io_object,
+		int *select_type)
+{
+	return EOPNOTSUPP;
+}
+
+kern_return_t
+lisp_io_stat(io_t stat_object,
+		io_statbuf_t *stat_info)
+{
+	return EOPNOTSUPP;
+}
+
+kern_return_t
+lisp_io_reauthenticate(io_t auth_object,
+		mach_port_t rendezvous2)
+{
+	return EOPNOTSUPP;
+}
+
+kern_return_t
+lisp_io_restrict_auth(io_t io_object,
+		mach_port_t *new_object,
+		mach_msg_type_name_t *new_objectPoly,
+		idarray_t uids,
+		mach_msg_type_number_t uidsCnt,
+		idarray_t gids,
+		mach_msg_type_number_t gidsCnt)
+{
+	return EOPNOTSUPP;
+}
+
+kern_return_t
+lisp_io_duplicate(io_t io_object,
+		mach_port_t *newport,
+		mach_msg_type_name_t *newportPoly)
+{
+	return EOPNOTSUPP;
+}
+
+kern_return_t
+lisp_io_server_version(io_t vers_object,
+		string_t server_name,
+		int *server_major_version,
+		int *server_minor_version,
+		int *server_edit_level)
+{
+	return EOPNOTSUPP;
+}
+
+kern_return_t
+lisp_io_map(io_t io_object,
+		mach_port_t *memobjrd,
+		mach_msg_type_name_t *memobjrdPoly,
+		mach_port_t *memobjwt,
+		mach_msg_type_name_t *memobjwtPoly)
+{
+	return EOPNOTSUPP;
+}
+
+kern_return_t
+lisp_io_map_cntl(io_t io_object,
+		mach_port_t *memobj,
+		mach_msg_type_name_t *memobjPoly)
+{
+	return EOPNOTSUPP;
+}
+
+kern_return_t
+lisp_io_get_conch(io_t io_object)
+{
+	return EOPNOTSUPP;
+}
+
+kern_return_t
+lisp_io_release_conch(io_t io_object)
+{
+	return EOPNOTSUPP;
+}
+
+kern_return_t
+lisp_io_eofnotify(io_t io_object)
+{
+	return EOPNOTSUPP;
+}
+
+kern_return_t
+lisp_io_prenotify(io_t io_object,
+		vm_offset_t write_start,
+		vm_offset_t write_end)
+{
+	return EOPNOTSUPP;
+}
+
+kern_return_t
+lisp_io_postnotify(io_t io_object,
+		vm_offset_t write_start,
+		vm_offset_t write_end)
+{
+	return EOPNOTSUPP;
+}
+
+kern_return_t
+lisp_io_readnotify(io_t io_object)
+{
+	return EOPNOTSUPP;
+}
+
+kern_return_t
+lisp_io_readsleep(io_t io_object)
+{
+	return EOPNOTSUPP;
+}
+
+kern_return_t
+lisp_io_sigio(io_t io_object)
+{
+	return EOPNOTSUPP;
+}
+
+kern_return_t
+lisp_io_pathconf(io_t io_object,
+		int name,
+		int *value)
+{
+	return EOPNOTSUPP;
+}
+
+kern_return_t
+lisp_io_identity(io_t io_object,
+		mach_port_t *idport,
+		mach_msg_type_name_t *idportPoly,
+		mach_port_t *fsidport,
+		mach_msg_type_name_t *fsidportPoly,
+		ino64_t *fileno)
+{
+	return EOPNOTSUPP;
+}
+
+kern_return_t
+lisp_io_revoke(io_t io_object)
+{
+	return EOPNOTSUPP;
+}
