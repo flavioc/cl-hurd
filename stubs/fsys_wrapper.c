1c02ea405df2d111abcc20a5e59b7bce68f35107
flavio@hurd <>
2008-06-11 01:39:10 +0000

    Create routines table and use it.

--- stubs/fsys_wrapper.c
+++ stubs/fsys_wrapper.c
@@ -20,21 +20,18 @@
 #include <sys/utsname.h>
 #include <hurd/hurd_types.h>
 
-/* routines that can be set by the middle level */
-typedef enum {
-	FSYS_STARTUP,
-	FSYS_GOAWAY,
-	FSYS_GETROOT,
-	FSYS_GETFILE,
-	FSYS_SYNCFS,
-	FSYS_SET_OPTIONS,
-	FSYS_GETPRIV,
-	FSYS_INIT,
-	FSYS_FORWARD,
-	FSYS_GET_OPTIONS
-} FsysRoutine;
+#include "fsys_wrapper.h"
+
+/* this is NULL initialized */
+static void* routines[_NUMBER_OF_ROUTINES];
 
 /* function wrappers follows... */
+
+/* fsys startup */
+
+typedef kern_return_t (*fsys_startup_type)(mach_port_t,
+		int, mach_port_t,
+		mach_port_t *, mach_msg_type_name_t *);
 
 kern_return_t
 lisp_fsys_startup(mach_port_t bootstrap,
@@ -43,14 +40,45 @@ lisp_fsys_startup(mach_port_t bootstrap,
 		mach_port_t *realnode,
 		mach_msg_type_name_t *realnodePoly)
 {
-	return EOPNOTSUPP;
+	if(routines[FSYS_STARTUP] == NULL) {
+		return EOPNOTSUPP;
+	}
+
+	fsys_startup_type startup_routine =
+		routines[FSYS_STARTUP];
+
+	return startup_routine(bootstrap, openflags,
+			control_port, realnode, realnodePoly);
 }
+
+/* fsys goaway */
+
+typedef kern_return_t (*fsys_goaway_type)(fsys_t, int);
 
 kern_return_t
 lisp_fsys_goaway(fsys_t fsys, int flags)
 {
-	return EOPNOTSUPP;
+	if(routines[FSYS_GOAWAY] == NULL) {
+		return EOPNOTSUPP;
+	}
+
+	fsys_goaway_type goaway_routine = routines[FSYS_GOAWAY];
+
+	return goaway_routine(fsys, flags);
 }
+
+/* fsys getroot */
+
+typedef kern_return_t (*fsys_getroot_type)(fsys_t,
+		mach_port_t,
+		idarray_t,
+		mach_msg_type_number_t,
+		idarray_t,
+		mach_msg_type_number_t,
+		int, retry_type *,
+		string_t,
+		mach_port_t *,
+		mach_msg_type_name_t *);
 
 kern_return_t
 lisp_fsys_getroot(fsys_t fsys,
@@ -65,8 +93,32 @@ lisp_fsys_getroot(fsys_t fsys,
 		mach_port_t *file,
 		mach_msg_type_name_t *filePoly)
 {
-	return EOPNOTSUPP;
+	if(routines[FSYS_GETROOT] == NULL) {
+		return EOPNOTSUPP;
+	}
+
+	fsys_getroot_type getroot_routine =
+		routines[FSYS_GETROOT];
+
+	return getroot_routine(fsys, dotdot_node,
+			gen_uids, gen_uidsCnt,
+			gen_gids, gen_gidsCnt,
+			flags, do_retry,
+			retry_name, file,
+			filePoly);
 }
+
+/* fsys getfile */
+
+typedef kern_return_t (*fsys_getfile_type)(fsys_t,
+		idarray_t,
+		mach_msg_type_number_t,
+		idarray_t,
+		mach_msg_type_number_t,
+		data_t,
+		mach_msg_type_number_t,
+		mach_port_t *,
+		mach_msg_type_name_t *);
 
 kern_return_t
 lisp_fsys_getfile(fsys_t fsys,
@@ -79,16 +131,42 @@ lisp_fsys_getfile(fsys_t fsys,
 		mach_port_t *file,
 		mach_msg_type_name_t *filePoly)
 {
-	return EOPNOTSUPP;
+	if(routines[FSYS_GETFILE] == NULL) {
+		return EOPNOTSUPP;
+	}
+
+	fsys_getfile_type getfile_routine =
+		routines[FSYS_GETFILE];
+
+	return getfile_routine(fsys, gen_uids, gen_uidsCnt,
+			gen_gids, gen_gidsCnt,
+			filehandle, filehandleCnt,
+			file, filePoly);
 }
+
+/* fsys syncfs */
+
+typedef kern_return_t (*fsys_syncfs_type)(fsys_t,
+		int, int);
 
 kern_return_t
 lisp_fsys_syncfs(fsys_t fsys,
 		int wait,
 		int do_children)
 {
-	return EOPNOTSUPP;
+	if(routines[FSYS_SYNCFS] == NULL) {
+		return EOPNOTSUPP;
+	}
+
+	fsys_syncfs_type syncfs_routine = routines[FSYS_SYNCFS];
+
+	return syncfs_routine(fsys, wait, do_children);
 }
+
+/* fsys set options */
+
+typedef kern_return_t (*fsys_set_options_type)(fsys_t,
+		data_t, mach_msg_type_number_t, int);
 
 kern_return_t
 lisp_fsys_set_options(fsys_t fsys,
@@ -96,8 +174,26 @@ lisp_fsys_set_options(fsys_t fsys,
 		mach_msg_type_number_t optionsCnt,
 		int do_children)
 {
-	return EOPNOTSUPP;
+	if(routines[FSYS_SET_OPTIONS] == NULL) {
+		return EOPNOTSUPP;
+	}
+
+	fsys_set_options_type set_options_routine =
+		routines[FSYS_SET_OPTIONS];
+
+	return set_options_routine(fsys, options,
+			optionsCnt, do_children);
 }
+
+/* fsys getpriv */
+
+typedef kern_return_t (*fsys_getpriv_type)(fsys_t,
+		mach_port_t *,
+		mach_msg_type_name_t *,
+		mach_port_t *,
+		mach_msg_type_name_t *,
+		mach_port_t *,
+		mach_msg_type_name_t *);
 
 kern_return_t
 lisp_fsys_getpriv(fsys_t fsys,
@@ -108,8 +204,24 @@ lisp_fsys_getpriv(fsys_t fsys,
 		mach_port_t *fstask,
 		mach_msg_type_name_t *fstaskPoly)
 {
-	return EOPNOTSUPP;
+	if(routines[FSYS_GETPRIV] == NULL) {
+		return EOPNOTSUPP;
+	}
+
+	fsys_getpriv_type getpriv_routine =
+		routines[FSYS_GETPRIV];
+
+	return getpriv_routine(fsys, host_priv,
+			host_privPoly, device_master,
+			device_masterPoly, fstask,
+			fstaskPoly);
 }
+
+/* fsys init */
+
+typedef kern_return_t (*fsys_init_type)(fsys_t,
+		mach_port_t, mach_msg_type_name_t,
+		mach_port_t, auth_t);
 
 kern_return_t
 lisp_fsys_init(fsys_t fsys,
@@ -118,8 +230,21 @@ lisp_fsys_init(fsys_t fsys,
 		mach_port_t proc_server,
 		auth_t auth_handle)
 {
-	return EOPNOTSUPP;
+	if(routines[FSYS_INIT] == NULL) {
+		return EOPNOTSUPP;
+	}
+
+	fsys_init_type init_routine = routines[FSYS_INIT];
+
+	return init_routine(fsys, reply_port, reply_portPoly,
+			proc_server, auth_handle);
 }
+
+/* fsys forward */
+
+typedef kern_return_t (*fsys_forward_type)(mach_port_t,
+		mach_port_t, data_t,
+		mach_msg_type_number_t);
 
 kern_return_t
 lisp_fsys_forward(mach_port_t server,
@@ -127,13 +252,37 @@ lisp_fsys_forward(mach_port_t server,
 		data_t argv,
 		mach_msg_type_number_t argvCnt)
 {
-	return EOPNOTSUPP;
+	if(routines[FSYS_FORWARD] == NULL) {
+		return EOPNOTSUPP;
+	}
+
+	fsys_forward_type forward_routine = routines[FSYS_FORWARD];
+
+	return forward_routine(server, requestor, argv, argvCnt);
 }
+
+/* fsys get options */
+
+typedef kern_return_t (*fsys_get_options_type)(fsys_t,
+		data_t *, mach_msg_type_number_t *);
 
 kern_return_t
 lisp_fsys_get_options(fsys_t server,
 		data_t *options,
 		mach_msg_type_number_t *optionsCnt)
 {
-	return EOPNOTSUPP;
+	if(routines[FSYS_GET_OPTIONS] == NULL) {
+		return EOPNOTSUPP;
+	}
+
+	fsys_get_options_type get_options_routine =
+		routines[FSYS_GET_OPTIONS];
+
+	return get_options_routine(server, options, optionsCnt);
+}
+
+void
+set_fsys_routine(const FsysRoutine what, void *fun)
+{
+	routines[what] = fun;
 }
