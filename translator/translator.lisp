a0ceab7a761bc3622e0b31e59c1d8a4b82aaf3c7
flavio@hurd <>
2008-06-13 13:34:00 +0000

    Load new files.

--- translator/translator.lisp
+++ translator/translator.lisp
@@ -1,3 +1,9 @@
 (in-package :hurd-translator)
 
+(load "translator/globals")
 (load "translator/io_wrapper")
+(load "translator/fs_wrapper")
+(load "translator/fsys_wrapper")
+(load "translator/macros")
+(load "translator/debug")
+
