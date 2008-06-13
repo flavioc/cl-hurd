06bb8dd2e6b4a113ac2ded14665067cfadd5e2c3
flavio@hurd <>
2008-06-13 13:30:39 +0000

    Lisp wrapper for fsys interfaces.

---
+++ translator/fsys_wrapper.lisp
@@ -0,0 +1,26 @@
+
+(define-stub-library fsys)
+
+(defcenum fsys-routine-code
+	:FSYS-STARTUP
+	:FSYS-GOAWAY
+	:FSYS-GETROOT
+	:FSYS-GETFILE
+	:FSYS-SYNCFS
+	:FSYS-SET-OPTIONS
+	:FSYS-GETPRIV
+	:FSYS-INIT
+	:FSYS-FORWARD
+	:FSYS-GET_OPTIONS)
+
+(defcfun ("set_fsys_routine" %set-fsys-routine) :void
+  (what fsys-routine-code)
+  (fun :pointer))
+
+(defun set-fsys-routine (what fun)
+  (declare (type symbol what))
+  (%set-fsys-routine what fun))
+
+(defsetf fsys-routine set-fsys-routine)
+
+(defcfun ("get_fsys_info" %get-fsys-info) :void)
