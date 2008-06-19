d6e9c7bd429ec3c2f36c554fbcc48370c1db64cd
cruz@localhost.localdomain <>
2008-06-19 16:42:35 +0000

    Add constant +auth-server+

--- translator/globals.lisp
+++ translator/globals.lisp
@@ -4,3 +4,8 @@
 ; the *translator* methods
 (defvar *translator* nil)
 
+; port name of current authentication server
+(defconstant +auth-server+ (getauth))
+
+(if (not (port-valid +auth-server+))
+  (error "Could not get a valid port name to the authentication server"))
