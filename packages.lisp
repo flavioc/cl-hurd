de863e8a0283724d3a5257ed3c67c3592783af54
root@(null) <>
2008-06-20 15:19:50 +0000

    Export new symbols.

--- packages.lisp
+++ packages.lisp
@@ -8,20 +8,37 @@
 		   :num-bits
 		   :define-helper-library
 		   :define-stub-library
-		   :string->integer
 		   :error->string
-		   :err))
+		   :err
+		   :unless-return
+		   :translate-foreign-list
+		   :with-gensyms
+		   :select-error))
 
 (defpackage :cl-mach
   (:nicknames :mach)
   (:use :cl :cffi :hurd-common)
   (:export :task-self
-		   :with-port))
+	   :with-port
+	   :port
+	   :port-valid
+	   :port-allocate
+	   :port-deallocate
+	   :port-destroy
+	   :port-mscount
+	   :msg-seqno
+	   :get-bootstrap-port))
 
 (defpackage :cl-hurd
   (:nicknames :hurd)
-  (:use :cl :cffi :mach :hurd-common))
+  (:use :cl :cffi :mach :hurd-common :tg)
+  (:export :getauth
+		   :ports-create-class
+		   :ports-create-bucket
+		   :create-port
+		   :get-send-right
+		   ))
 
 (defpackage :cl-hurd.translator
   (:nicknames :hurd-translator)
-  (:use :cl :cffi :mach :hurd-common :hurd))
+  (:use :cl :cffi :mach :hurd-common :hurd :tg))
