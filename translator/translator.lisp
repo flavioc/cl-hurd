2adc246b4a79b179ed8f7f7013bf0bf1d98c2345
root@(null) <>
2008-06-20 15:19:18 +0000

    Load demuxer.

--- translator/translator.lisp
+++ translator/translator.lisp
@@ -6,6 +6,7 @@
 (load "translator/fsys_wrapper")
 (load "translator/macros")
 (load "translator/debug")
+(load "translator/demuxer")
 (load "translator/class")
 (load "translator/node")
 (load "translator/dir")
