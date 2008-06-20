4fe3f57c4d03a20aed8ff8accb5ee1207e59de9e
root@(null) <>
2008-06-20 15:16:05 +0000

    Load libports and libfshelp

--- hurd/hurd.lisp
+++ hurd/hurd.lisp
@@ -1,4 +1,13 @@
+
 (in-package :hurd)
+
+(define-foreign-library libfshelp
+	(t (:default "libfshelp")))
+(use-foreign-library libfshelp)
+
+(define-foreign-library libports
+	(t (:default "libports")))
+(use-foreign-library libfshelp)
 
 (load "hurd/types")
 (load "hurd/functions")
