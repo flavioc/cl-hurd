a9180231b957c24951222f44e62d790648ecf105
root@localhost <>
2008-06-27 20:02:12 +0100

    Load new implemented symbols.

--- packages.lisp
+++ packages.lisp
@@ -3,7 +3,7 @@
 
 (defpackage :cl-hurd.common
   (:nicknames :hurd-common)
-  (:use :cl :cffi)
+  (:use :cl :cffi :tg)
   (:export :largest-representable-number
 		   :num-bits
 		   :define-helper-library
@@ -14,6 +14,8 @@
 		   :translate-foreign-list
 		   :with-gensyms
 		   :select-error
+		   :open-flags-t
+		   :mode-t
 		   :open-flags
 		   :with-cleanup))
 
@@ -21,29 +23,41 @@
   (:nicknames :mach)
   (:use :cl :cffi :hurd-common)
   (:export :task-self
+	   :with-port-deallocate
+	   :with-port-destroy
 	   :with-port
+	   :with-send-right
+	   :with-receive-right
 	   :port
+	   :is
 	   :port-pointer
 	   :port-valid
 	   :port-allocate
 	   :port-deallocate
 	   :port-destroy
+	   :port-mod-refs
+	   :port-move-member
+	   :port-insert-right
+	   :port-type
+	   :port-request-notification
 	   :port-mscount
 	   :msg-seqno
 	   :get-bootstrap-port
-	   :msg-type-name))
+	   :msg-type-name
+	   :msg-server-timeout))
 
 (defpackage :cl-hurd
   (:nicknames :hurd)
   (:use :cl :cffi :mach :hurd-common :tg)
   (:export :getauth
-		   :ports-create-class
-		   :ports-create-bucket
-		   :ports-manage-operations-one-thread
-		   :create-port
 		   :get-send-right
-		   :with-port-info
 		   :fsys-startup
+		   :make-bucket
+		   :run-server
+		   :add-port
+		   :has-port
+		   :+servers-exec+
+		   :file-name-lookup
 		   ))
 
 (defpackage :cl-hurd.translator
