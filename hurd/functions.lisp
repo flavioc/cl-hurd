3b6e510ae46828b46af49fd7b9b47c3229d0706d
root@localhost <>
2008-06-27 19:30:28 +0100

    Add file-name-lookup

--- hurd/functions.lisp
+++ hurd/functions.lisp
@@ -3,3 +3,12 @@
 
 (defun getauth ()
   (%getauth))
+
+(defcfun ("file_name_lookup" %file-name-lookup)
+	 port
+	 (name :string)
+	 (flags open-flags-t)
+	 (mode mode-t))
+
+(defun file-name-lookup (name &optional (flags nil) (mode nil))
+  (%file-name-lookup name flags mode))
