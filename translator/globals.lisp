e7c71e96bac68008bbf89afd000bb6eeff2631f0
root@localhost <>
2008-06-27 20:01:09 +0100

    Add port to the exec server.

--- translator/globals.lisp
+++ translator/globals.lisp
@@ -4,8 +4,13 @@
 ; the *translator* methods
 (defvar *translator* nil)
 
-; port name of current authentication server
+; port of current authentication server
 (defconstant +auth-server+ (getauth))
+
+; port of the current exec server (for file_exec interface)
+(defconstant +exec-server+ (file-name-lookup +servers-exec+))
 
 (if (not (port-valid +auth-server+))
   (error "Could not get a valid port name to the authentication server"))
+(if (not (port-valid +exec-server+))
+  (error "Could not get a valid port name to the exec server"))
