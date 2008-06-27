6c2ceead4cb8fc663f4ac9fe310fd9ee4d99e59b
root@localhost <>
2008-06-27 19:59:28 +0100

    Load new files. Do not load libports.

--- hurd/hurd.lisp
+++ hurd/hurd.lisp
@@ -6,11 +6,10 @@
 	(t (:default "libfshelp")))
 (use-foreign-library libfshelp)
 
-(define-foreign-library libports
-	(t (:default "libports")))
-(use-foreign-library libfshelp)
-
 (load "hurd/types")
 (load "hurd/functions")
 (load "hurd/ports/ports")
 (load "hurd/fsys/fsys")
+(load "hurd/io/io")
+(load "hurd/iohelp/iohelp")
+(load "hurd/paths")
