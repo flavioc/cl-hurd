18b6e16c95859e38ef7e004eedcbc35591a0bfdd
Flavio Cruz <flaviocruz@gmail.com>
2008-07-02 21:11:08 +0100

    Add getuid, geteuid, getgid and getegid.

--- common/functions.lisp
+++ common/functions.lisp
@@ -1,6 +1,21 @@
 
-(defcfun ("strerror" %strerror) :string
-		 (code err))
+(defcfun ("strerror" %strerror) :string (code err))
 
 (defun error->string (error-code)
   (%strerror error-code))
+
+(defcfun ("getuid" %getuid) :unsigned-int)
+
+(defun getuid () (%getuid))
+
+(defcfun ("geteuid" %geteuid) :unsigned-int)
+
+(defun geteuid () (%geteuid))
+
+(defcfun ("getgid" %getgid) :unsigned-int)
+
+(defun getgid () (%getgid))
+
+(defcfun ("getegid" %getegid) :unsigned-int)
+
+(defun getegid () (%getegid))
