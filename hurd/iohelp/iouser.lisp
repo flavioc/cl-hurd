b125eb92835df66c356c4b892bd72d5c49b62ae6
Flavio Cruz <flaviocruz@gmail.com>
2008-07-02 21:28:06 +0100

    Add contains-uid, contains-gid, empty-uids-p, empty-gids-p and print-object to iouser. Support when creating a new iouser object in make-iouser.

--- hurd/iohelp/iouser.lisp
+++ hurd/iohelp/iouser.lisp
@@ -10,23 +10,36 @@
 	 :documentation "List of gids"))
   (:documentation "Groups list of uids and gids representing an I/O user"))
 
-(defun make-iouser (&key (uids nil) (gids nil))
+(defun make-iouser (&key (uids nil) (gids nil) (old nil))
   "Create an iouser based on lists or atoms of uids and gids"
-  (make-instance 'iouser
-		 :uids (if (listp uids) uids (list uids))
-		 :gids (if (listp gids) gids (list gids))))
+  (if old
+	(make-instance 'iouser
+				   :uids (uids old)
+				   :gids (gids old))
+	(make-instance 'iouser
+				   :uids (if (listp uids) uids (list uids))
+				   :gids (if (listp gids) gids (list gids)))))
 
 (defun make-iouser-mem (uids-ptr uids-len gids-ptr gids-len)
   "Create an iouser based on foreign pointers"
   (let ((obj (make-instance 'iouser
 			    :uids (idptr-to-list uids-ptr uids-len)
 			    :gids (idptr-to-list gids-ptr gids-len))))
-    (free-ptr uids-ptr uids-len)
-    (free-ptr gids-ptr gids-len)
+;    (free-ptr uids-ptr uids-len)
+ ;   (free-ptr gids-ptr gids-len)
     obj))
 
-(defun contains-uid ((iouser iouser) uid)
+(defmethod contains-uid ((iouser iouser) uid)
   (member uid (uids iouser)))
+
+(defmethod contains-gid ((iouser iouser) gid)
+  (member gid (gids iouser)))
+
+(defmethod empty-uids-p ((iouser iouser))
+  (null (uids iouser)))
+
+(defmethod empty-gids-p ((iouser iouser))
+  (null (gids iouser)))
 
 (defmethod restrict-iouser ((user1 iouser) (user2 iouser))
   (cond
@@ -36,4 +49,5 @@
 				       (uids user2))
 		   :gids (intersection (gids user1)
 				       (gids user2))))))
-
+(defmethod print-object ((iouser iouser) stream)
+  (format stream "#<iouser: uids -> ~s ; gids -> ~s>" (uids iouser) (gids iouser)))
