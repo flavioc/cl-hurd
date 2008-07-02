991db58fe0b8ad92151218ddb360e9a247d719f4
Flavio Cruz <flaviocruz@gmail.com>
2008-07-02 21:23:46 +0100

    Load files.

---
+++ hurd/fshelp/fshelp.lisp
@@ -0,0 +1,8 @@
+
+(load "hurd/fshelp/touch")
+(load "hurd/fshelp/isowner")
+(load "hurd/fshelp/iscontroller")
+(load "hurd/fshelp/access")
+(load "hurd/fshelp/checkdirmod")
+(load "hurd/fshelp/fetch-root")
+(load "hurd/fshelp/transbox")
