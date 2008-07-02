0639bd1f587693be6e2aabba292b8939d2e75fb9
Flavio Cruz <flaviocruz@gmail.com>
2008-07-02 21:29:51 +0100

    Add a cleanup function to be run when a new port goes out of the bucket. Add remove-port. Add lookup-port. Add add-port and add-new-port. Add a global ports hash table to be used with incoming mach msg events.

--- hurd/ports/bucket.lisp
+++ hurd/ports/bucket.lisp
@@ -1,21 +1,34 @@
 
+(defvar *all-ports* (make-hash-table))
+
 (defclass port-bucket ()
   ((port-set :initform (port-allocate :right-port-set)
 	     :accessor port-set
 	     :documentation "A port set containing all the ports in the bucket")
+   (cleanup :initform nil
+			:initarg :cleanup
+			:accessor cleanup-function
+			:documentation "To be run when a port is not used anymore")
    (hash :initform (make-hash-table)
-	 :accessor table
-	 :documentation "Contains the ports in this bucket")))
+		 :accessor table
+		 :documentation "Contains the ports in this bucket")))
 
 (defmethod cleanup ((bucket port-bucket))
   (with-accessors ((port port-set)) bucket
     (port-destroy port)))
 
-(defmethod add-port ((bucket port-bucket))
+(defmethod add-new-port ((bucket port-bucket))
   (let ((port (make-instance 'port-info)))
-    (move-receive-right bucket (port-right port))
-    (setf (gethash (port-right port) (table bucket)) port)
-    port))
+    (add-port bucket port)))
+
+(defmethod add-port ((bucket port-bucket) (port port-info))
+  (move-receive-right bucket (port-right port))
+  (setf (gethash (port-right port) (table bucket)) port)
+  ;; also add this port to *all-ports* so we can look
+  ;; it up when a no-senders notification arrives
+  (setf (gethash (port-right port) *all-ports*)
+		(list port bucket))
+  port)
 
 (defmethod move-receive-right ((bucket port-bucket) right)
   (port-move-member right
@@ -26,5 +39,17 @@
     (multiple-value-bind (val found) (gethash port table)
       found)))
 
-(defun make-bucket ()
-  (make-instance 'port-bucket))
+(defmethod lookup-port ((bucket port-bucket) port)
+  (with-accessors ((table table)) bucket
+	(gethash port table)))
+
+(defmethod remove-port ((bucket port-bucket) port)
+  (with-accessors ((cleanup-fun cleanup-function)) bucket
+	(remhash (port-right port) (table bucket))
+	(set-send-rights port nil)
+	(if cleanup-fun
+	  (funcall cleanup-fun port))
+	(port-cleanup port)))
+
+(defun make-bucket (cleanup)
+  (make-instance 'port-bucket :cleanup cleanup))
