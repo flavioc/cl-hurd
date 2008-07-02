58c41831c17ddb8ccdb4499d3076c65906fed776
Flavio Cruz <flaviocruz@gmail.com>
2008-07-02 21:42:00 +0100

    Add file-utimes callback.

---
+++ translator/interfaces/file-utimes.lisp
@@ -0,0 +1,11 @@
+
+(def-fs-interface :file-utimes ((file port)
+								(atimein time-value-t)
+								(mtimein time-value-t))
+  (with-lookup protid file
+	(file-utimes *translator*
+				 (get-node protid)
+				 (get-user protid)
+				 atimein
+				 mtimein)))
+
