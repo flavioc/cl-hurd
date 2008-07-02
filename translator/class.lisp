9b1cedd5fb70a1f1410cc45caa0bcc0777ef7650
Flavio Cruz <flaviocruz@gmail.com>
2008-07-02 21:39:20 +0100

    Move start code to run.lisp

--- translator/class.lisp
+++ translator/class.lisp
@@ -7,14 +7,39 @@
 
 (defclass translator ()
   ((identity-port :initform (get-identity-port)
-		  :accessor identity-port)
-   (port-bucket :initform (make-bucket)
-		:accessor port-bucket)
-   (underlying-node :initform nil)
+				  :accessor identity-port)
+   (port-bucket :initform nil
+				:accessor port-bucket)
+   (temporary-bucket :initform (make-hash-table)
+					 :accessor temporary-bucket)
+   (underlying-node :initform nil
+					:reader underlying-node)
+   (root-node :initform nil
+			  :accessor root)
    (name :initform "cl-translator"
-	 :accessor name)
+		 :accessor name)
    (version :initform (list 1 0 0)
-	    :accessor version)))
+			:accessor version)))
+
+(defmethod initialize-instance :after ((translator translator) &key)
+  (setf (port-bucket translator)
+		(make-bucket (lambda (protid) nil)
+					   ;(warn "protid release")))))
+					   )))
+
+(defmethod insert-temporary-data ((trans translator) key value)
+  (setf (gethash (temporary-bucket trans) key) value))
+
+(defmethod get-temporary-data ((trans translator) key)
+  (with-cleanup (remove-temporary-data trans key)
+				(gethash (temporary-bucket trans) key)))
+
+(defmethod remove-temporary-data ((trans translator) key)
+  (remhash (temporary-bucket trans) key))
+
+(defmethod new-protid ((trans translator) user (open-node open-node))
+  (add-port (port-bucket trans)
+			(make-protid user open-node)))
 
 (defun create-translator (&optional (base-class 'translator))
   (let ((translator (make-instance base-class)))
@@ -25,34 +50,8 @@
 
 (defmethod setup ((trans translator) &optional (flags nil))
   (with-port-deallocate (bootstrap (get-bootstrap-port))
-    (let ((port (add-port (port-bucket trans))))
+    (let ((port (add-new-port (port-bucket trans))))
       (with-port-deallocate (right (get-send-right port))
         (let ((file (fsys-startup bootstrap flags right :copy-send)))
-	  (warn "file ~a~%" file)
-          (setf (slot-value trans 'underlying-node) file)
+		  (setf (slot-value trans 'underlying-node) file)
 	  t)))))
-
-(defmethod run ((trans translator))
-  (let ((*translator* trans))
-    (run-server (lambda (port in out)
-		  (if (has-port (port-bucket trans) port)
-		    (warn "port found~%")
-		    (warn "port not found~%"))
-		  (translator-demuxer in out))
-		(port-bucket trans))))
-
-
-(def-fs-interface :file-statfs ((file :int) (buf :pointer))
-		  (print "statfs")
-		  :operation-not-supported)
-
-(defparameter *mytranslator* (create-translator))
-
-(when (setup *mytranslator*)
-  (run *mytranslator*))
-
-;; example:
-;; (defclass zip-translator ()
-;;   ((name :initform "zip-translator")
-;;    (version :initform "1")))
-
