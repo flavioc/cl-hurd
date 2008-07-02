e161653f365351a14cdbe436b6c48a6ef224b79a
Flavio Cruz <flaviocruz@gmail.com>
2008-07-02 21:24:06 +0100

    Add fsys-getroot

--- hurd/fsys/functions.lisp
+++ hurd/fsys/functions.lisp
@@ -16,3 +16,31 @@
 		 type
 		 port)))
       (select-error err (mem-ref port 'port)))))
+
+(defcfun ("fsys_getroot" %fsys-getroot)
+		 err
+		 (control port)
+		 (dotdot port)
+		 (dotdot-poly msg-type-name)
+		 (uids :pointer)
+		 (uids-count msg-type-number)
+		 (gids :pointer)
+		 (gids-count msg-type-number)
+		 (flags :int)
+		 (do-retry :pointer)
+		 (retry-name :string)
+		 (file port-pointer))
+
+(defun fsys-getroot (control dotdot dotdot-poly
+							 uids uids-count
+							 gids gids-count
+							 flags
+							 do-retry retry-name)
+  (with-foreign-ptr (control (foreign-type-size 'pointer))
+	(let ((return-code (%fsys-getroot control dotdot
+									  dotdot-poly
+									  uids uids-count
+									  gids gids-count
+									  flags do-retry
+									  retry-name control)))
+	  (select-error return-code (mem-ref control 'port)))))
