4d4cf10354fc3a7ae19637a1697be8b083aa0700
Flavio Cruz <flaviocruz@gmail.com>
2008-07-02 21:36:49 +0100

    Add make-open-node.

--- translator/open.lisp
+++ translator/open.lisp
@@ -1,10 +1,29 @@
 
 (defclass open-node ()
-  ((node :initarg :refers)
+  ((node :initarg :refers
+		 :accessor refers)
    (file-pos :initform 0)
    (lock-status :initform 'unlock) ; /usr/include/sys/file.h
-   (openstat)
-   (root-parent :initform nil)
-   (shadow-root :initform nil)
-   (shadow-root-parent :initform nil)))
+   (openstat :initform nil
+			 :initarg :flags
+			 :accessor flags)
+   (root-parent :initform nil
+				:accessor root-parent)
+   (shadow-root :initform nil
+				:accessor shadow-root)
+   (shadow-root-parent :initform nil
+					   :accessor shadow-root-parent)))
 
+(defun make-open-node (node flags
+							&key (root-parent nil)
+								 (shadow-root nil)
+								 (shadow-root-parent nil))
+  (let ((obj (make-instance 'open-node :refers node :flags flags)))
+	(setf (root-parent obj) root-parent)
+	(if (port-valid (root-parent obj))
+	  (port-mod-refs (root-parent obj) :right-send 1))
+	(setf (shadow-root obj) shadow-root)
+	(setf (shadow-root-parent obj) shadow-root-parent)
+	(if (port-valid shadow-root-parent)
+	  (port-mod-refs shadow-root-parent :right-send 1))
+	obj))
