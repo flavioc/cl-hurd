52305bf3f8ef5112c02afee8ad56465346123b40
Flavio Cruz <flaviocruz@gmail.com>
2008-07-02 21:15:01 +0100

    Possible values to pathconf().

---
+++ common/pathconf.lisp
@@ -0,0 +1,23 @@
+
+(defcenum pathconf-type
+		  :link-max
+		  :max-canon
+		  :max-input
+		  :name-max
+		  :path-max
+		  :pipe-buf
+		  :chown-restricted
+		  :no-trunc
+		  :vdisable
+		  :sync-io
+		  :prio-io
+		  :sock-maxbuf
+		  :filesizebits
+		  :rec-incr-xfer-size
+		  :rec-max-xfer-size
+		  :rec-min-xfer-size
+		  :rec-xfer-align
+		  :alloc-size-min
+		  :symlink-max
+		  :2-symlinks)
+
