59e69681f3576d921dd1391f511fdc886ed28ce6
Flavio Cruz <flaviocruz@gmail.com>
2008-07-02 21:38:15 +0100

    Add box and references fields, remove left and right fields. Add initialize-instance to add a new transbox.

--- translator/node.lisp
+++ translator/node.lisp
@@ -1,7 +1,14 @@
 
 (defclass node ()
-  ((left-node :initform nil)
-   (right-node :initform nil)
-   (owner :initform nil)
-   (io-stat) ;; FIXME
-   ))
+   ((owner :initform 0
+		   :initarg :owner
+		   :accessor owner)
+	(stat :initform (make-stat)
+		  :initarg :stat
+		  :accessor stat)
+	(references :initform 1)
+	(box :initform nil
+		 :accessor box)))
+
+(defmethod initialize-instance :after ((node node) &key)
+  (setf (box node) (make-transbox node)))
