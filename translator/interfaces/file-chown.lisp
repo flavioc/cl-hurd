2e5f5c680fa44e2f20befcf350c0713444313540
Flavio Cruz <flaviocruz@gmail.com>
2008-07-02 21:41:52 +0100

    Add file-chown callback.

---
+++ translator/interfaces/file-chown.lisp
@@ -0,0 +1,10 @@
+
+(def-fs-interface :file-chown ((file port)
+							   (owner uid-t)
+							   (group gid-t))
+  (with-lookup protid file
+	(file-chown *translator*
+				(get-node protid)
+				(get-user protid)
+				owner
+				group)))
