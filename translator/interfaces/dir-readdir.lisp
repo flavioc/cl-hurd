dd4587e55d8d2cb5a03da4b493793a3902fdc18f
Flavio Cruz <flaviocruz@gmail.com>
2008-07-02 21:41:07 +0100

    Add dir-readdir callback.

---
+++ translator/interfaces/dir-readdir.lisp
@@ -0,0 +1,134 @@
+
+(defconstant +chunk-size+ (* 8 1024))
+
+(defun %get-dirent-type (stat)
+  (cond
+    ((is-reg-p stat) :reg)
+    ((is-dir-p stat) :dir)
+    ((is-blk-p stat) :blk)
+    ((is-lnk-p stat) :lnk)
+    ((is-sock-p stat) :sock)
+    (t
+      :unknown)))
+
+(defcstruct dirent-struct
+	    (ino ino-t)
+	    (reclen :unsigned-short)
+	    (type :unsigned-char)
+	    (namlen :unsigned-char))
+
+(defconstant +name-offset+ (foreign-type-size 'dirent-struct))
+
+(defun %dir-readdir-count (dir-protid entry final-entry data initial-size limited-p)
+;  (warn "count1")
+  (let ((size initial-size)
+		(ptr data)
+		(total (- final-entry entry))
+		(entries (get-entries *translator* (get-node dir-protid) (get-user dir-protid) entry final-entry)))
+    ;(warn "count")
+    (loop for dirent in entries
+	  for cnt = 0 then (1+ cnt)
+	  do (let* ((dirent-name (name dirent))
+		    (namelen (1+ (length dirent-name)))
+		    (this-size (+ +name-offset+ namelen))
+		    (node (node dirent)))
+	       (when (> (+ this-size
+			   (- (pointer-address ptr)
+			      (pointer-address data)))
+			size)
+		 (warn "exceeded")
+		 (when limited-p
+		   (warn "limited! returning..")
+		   (return (values cnt ptr size)))
+		 (with-foreign-pointer (extension (foreign-type-size 'vm-address))
+				       (warn "extending..")
+				       (let ((result (vm-allocate extension +chunk-size+ 0)))
+					 (if (null result)
+					   (return (values nil nil size))
+					   (incf size +chunk-size+)))))
+	       (with-foreign-pointer (dirent-ptr this-size)
+				    ; (warn "name-offset: ~s node is ~s size: ~s ; this-size ~s~%"
+				;	   +name-offset+ dirent-name (+ +name-offset+ namelen) this-size)
+				     (setf (foreign-slot-value dirent-ptr 'dirent-struct 'ino) (stat-get (stat node) 'ino))
+				     (setf (foreign-slot-value dirent-ptr 'dirent-struct 'reclen) this-size)
+				     (setf (foreign-slot-value dirent-ptr 'dirent-struct 'type)
+					   (foreign-enum-value 'dirent-type (%get-dirent-type (stat node))))
+				     (setf (foreign-slot-value dirent-ptr 'dirent-struct 'namlen) namelen)
+				     (lisp-string-to-foreign dirent-name
+							     (inc-pointer dirent-ptr +name-offset+)
+							     namelen)
+				     (memcpy ptr dirent-ptr this-size)
+				    ; (warn "old pointer ~s~%" ptr)
+				     (incf-pointer ptr this-size)))
+				     ;(warn "pointer now to ~s~%" ptr)))
+	  when (= cnt total)
+	  return (progn
+		   ;(warn "returning count ~s ptr ~s size ~s~%" (1+ cnt) ptr size)
+		   (values (1+ cnt) ptr size)))))
+
+;(setf *trace-output* *error-output*)
+;(trace %dir-readdir-count)
+
+(defun %dir-readdir (dir-protid bufsiz entry nentries existing-entries)
+  ;(warn "%dir-readdir ~s ~s ~s ~s ~s ~%" dir-protid bufsiz entry nentries existing-entries)
+  (let* ((size (if (or (zerop bufsiz) (> bufsiz +chunk-size+))
+		 +chunk-size+
+		 bufsiz))
+	 (data (mmap (make-pointer 0) size '(:prot-read :prot-write) '(:map-anon) 0 0)))
+    (unless data
+      (return-from %dir-readdir nil))
+    (let* ((limited-p (> bufsiz 0))
+	   (final-entry (1- (if (= nentries -1)
+			      existing-entries
+			      (min (+ entry nentries) existing-entries)))))
+      (multiple-value-bind (cnt ptr new-size)
+	(%dir-readdir-count dir-protid entry final-entry data size limited-p)
+	(cond
+	  ((null cnt)
+	   ;(warn "fail to allocate")
+	   (unless (null new-size)
+	     (munmap data new-size))
+	   nil)
+	  (t
+	    ;(warn "start ptr ~s final ptr ~s total size ~s" data ptr new-size)
+	    (let ((alloc-end (pointer-address (inc-pointer data new-size)))
+		  (real-end (round-page (pointer-address ptr))))
+	      (when (> alloc-end real-end)
+		;(warn "munmap unused from ~s to ~s" real-end alloc-end)
+		(munmap (make-pointer real-end) (- alloc-end real-end)))
+	      (values data
+		      (- (pointer-address ptr) (pointer-address data))
+		      cnt))))))))
+
+;(trace %dir-readdir)
+(def-fs-interface :dir-readdir ((port port)
+				(data :pointer)
+				(data-count :pointer)
+				(data-dealloc :pointer)
+				(entry :int)
+				(nentries :int)
+				(bufsiz :unsigned-int)
+				(amount :pointer))
+   (with-lookup dir-protid port
+      (block dir-readdir
+	     (unless (is (get-open-flags dir-protid) 'read)
+	       (return-from dir-readdir :bad-fd))
+	     (unless (is-dir-p (get-stat dir-protid))
+	       (return-from dir-readdir :not-directory))
+	     (let ((existing-entries (number-of-entries *translator*
+							(get-node dir-protid)
+							(get-user dir-protid))))
+	       (when (> (1+ entry) existing-entries)
+		 (setf (mem-ref amount :int) 0)
+		 (return-from dir-readdir t))
+	       (multiple-value-bind (data-ptr len cnt)
+		 (%dir-readdir dir-protid bufsiz entry nentries existing-entries)
+		 (when (null data-ptr)
+		   (setf (mem-ref amount :int) 0)
+		   (return-from dir-readdir t))
+		 ;(warn "count: ~s ; data: ~s len: ~s ~%" cnt data-ptr len)
+		 (setf (mem-ref amount :int) cnt)
+		 (setf (mem-ref data :pointer) data-ptr)
+		 (setf (mem-ref data-count 'msg-type-number) len)
+		 (setf (mem-ref data-dealloc :int) 1)
+		 t)))))
