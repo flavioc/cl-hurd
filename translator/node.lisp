fab43a72d82557b5bd2f996dc9c3c78759196077
Flavio Cruz <flaviocruz@gmail.com>
2008-07-03 23:40:57 +0100

    Add accessor to references.

--- translator/node.lisp
+++ translator/node.lisp
@@ -6,9 +6,13 @@
 	(stat :initform (make-stat)
 		  :initarg :stat
 		  :accessor stat)
-	(references :initform 1)
+	(references :initform 1
+				:accessor references)
 	(box :initform nil
 		 :accessor box)))
 
 (defmethod initialize-instance :after ((node node) &key)
   (setf (box node) (make-transbox node)))
+
+(defmethod print-object ((node node) stream)
+  (format stream "#<node ref: ~s>" (references node)))
