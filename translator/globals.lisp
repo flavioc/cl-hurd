03bcfe0ad8d957629066ee65658bced1caf3a3db
Flavio Cruz <flaviocruz@gmail.com>
2008-07-03 23:46:05 +0100

    Do not create +auth-server+ and +exec-server+ for now.

--- translator/globals.lisp
+++ translator/globals.lisp
@@ -5,12 +5,12 @@
 (defvar *translator* nil)
 
 ; port of current authentication server
-(defconstant +auth-server+ (getauth))
+;(defconstant +auth-server+ (getauth))
 
 ; port of the current exec server (for file_exec interface)
-(defconstant +exec-server+ (file-name-lookup +servers-exec+))
+;(defconstant +exec-server+ (file-name-lookup +servers-exec+))
 
-(if (not (port-valid +auth-server+))
-  (error "Could not get a valid port name to the authentication server"))
-(if (not (port-valid +exec-server+))
-  (error "Could not get a valid port name to the exec server"))
+;(if (not (port-valid +auth-server+))
+;  (error "Could not get a valid port name to the authentication server"))
+;(if (not (port-valid +exec-server+))
+;  (error "Could not get a valid port name to the exec server"))
