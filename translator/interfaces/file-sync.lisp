ce0a4c7db7f4d0bd28fc5b4cc1161b9bac1eea06
Flavio Cruz <flaviocruz@gmail.com>
2008-07-03 23:38:52 +0100

    New callback file-sync.

---
+++ translator/interfaces/file-sync.lisp
@@ -0,0 +1,12 @@
+
+(def-fs-interface :file-sync ((port port)
+							  (wait :boolean)
+							  (omit-meta :boolean))
+  (with-lookup protid port
+	 (if (file-sync *translator*
+					(get-node protid)
+					(get-user protid)
+					wait
+					omit-meta)
+	   t
+	   nil)))
