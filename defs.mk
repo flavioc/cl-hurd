74d469ade46c6b7f3034446cec151aec6b876269
Flavio Cruz <flaviocruz@gmail.com>
2008-07-03 23:45:09 +0100

    Add hurd and mach directories.

--- defs.mk
+++ defs.mk
@@ -2,5 +2,6 @@
 CFLAGS = -Wall -Wextra -D_GNU_SOURCE=1
 LDFLAGS = -lmachuser -lc -lhurduser
 
-DEFS_DIRECTORY = /usr/include/hurd
-MIG = mig -prefix lisp_ -user /dev/null -header /dev/null -server
+HURD_DIRECTORY = /usr/include/hurd
+MACH_DIRECTORY = /usr/include/mach
+MIG = mig -v -prefix lisp_ -user /dev/null -header /dev/null -server
