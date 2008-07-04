19afcb821d47439e7d15c3a9ba1f25267a1b7590
Flavio Cruz <flaviocruz@gmail.com>
2008-07-04 15:47:48 +0100

    Always return a port right.

--- hurd/ports/port.lisp
+++ hurd/ports/port.lisp
@@ -17,11 +17,13 @@
 (defmethod get-right ((port port-info))
   (incf (mscount port))
   (unless (has-send-rights port)
-    (with-accessors ((right port-right) (count mscount)) port
-      (port-request-notification right :notify-no-senders
-				 count right :make-send-once)
-      (setf (slot-value port 'has-send-rights) t)
-	  right)))
+	(set-send-rights port t)
+	(port-request-notification (port-right port)
+							   :notify-no-senders
+							   (mscount port)
+							   (port-right port)
+							   :make-send-once))
+  (port-right port))
 
 (defmethod get-send-right ((port port-info))
   (let ((right (get-right port)))
