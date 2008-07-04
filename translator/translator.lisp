8182e8a4d05545c93417e1691fd752df2e0dd8f5
Flavio Cruz <flaviocruz@gmail.com>
2008-07-04 16:18:43 +0100

    Rename files with underscore.

--- translator/translator.lisp
+++ translator/translator.lisp
@@ -1,9 +1,9 @@
 (in-package :hurd-translator)
 
 (load "translator/globals")
-(load "translator/io_wrapper")
-(load "translator/fs_wrapper")
-(load "translator/fsys_wrapper")
+(load "translator/io-wrapper")
+(load "translator/fs-wrapper")
+(load "translator/fsys-wrapper")
 (load "translator/macros")
 (load "translator/debug")
 (load "translator/demuxer")
