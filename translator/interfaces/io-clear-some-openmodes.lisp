ad2bd06d89a74ade2aa0c37969597588ca3b6c29
Flavio Cruz <flaviocruz@gmail.com>
2008-07-04 16:05:32 +0100

    Add io-clear-some-openmodes callback.

---
+++ translator/interfaces/io-clear-some-openmodes.lisp
@@ -0,0 +1,8 @@
+
+(def-io-interface :io-clear-some-openmodes ((port port)
+											(bits open-flags-t))
+   (with-lookup protid port
+	  (disable (flags (open-node protid))
+			   bits)
+	  t))
+
