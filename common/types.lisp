fca0fbc1ca217a264739ac9a0739aee9293b2953
Flavio Cruz <flaviocruz@gmail.com>
2008-07-06 19:03:49 +0000

    Comment.

--- common/types.lisp
+++ common/types.lisp
@@ -1,8 +1,11 @@
 
-(defctype off-t :unsigned-int)
+(in-package :hurd-common)
 
-(defctype loff-t :long-long)
+(defctype off-t :unsigned-int "C type off_t")
 
-(defctype ino-t :long-long)
+(defctype loff-t :long-long "C type loff_t")
 
-(defctype pid-t :int)
+(defctype ino-t :long-long "C type ino_t")
+
+(defctype pid-t :int "C type pid_t")
+
