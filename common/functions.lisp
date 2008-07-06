13b4cb8a67cfbc006b48ab266d0b475167a6f8ff
Flavio Cruz <flaviocruz@gmail.com>
2008-07-06 19:00:42 +0000

    Comment functions.

--- common/functions.lisp
+++ common/functions.lisp
@@ -1,21 +1,37 @@
 
+(in-package :hurd-common)
+
+;;
+;; This files declares some foreign functions that
+;; still don't deserve a single file for each one.
+;;
+
 (defcfun ("strerror" %strerror) :string (code err))
 
 (defun error->string (error-code)
+  "Translates an error code to a string."
   (%strerror error-code))
 
 (defcfun ("getuid" %getuid) :unsigned-int)
 
-(defun getuid () (%getuid))
+(defun getuid ()
+  "Get the real user ID of the process."
+  (%getuid))
 
 (defcfun ("geteuid" %geteuid) :unsigned-int)
 
-(defun geteuid () (%geteuid))
+(defun geteuid ()
+  "Get the effective user ID of the process."
+  (%geteuid))
 
 (defcfun ("getgid" %getgid) :unsigned-int)
 
-(defun getgid () (%getgid))
+(defun getgid ()
+  "Get the real group ID of the process."
+  (%getgid))
 
 (defcfun ("getegid" %getegid) :unsigned-int)
 
-(defun getegid () (%getegid))
+(defun getegid ()
+  "Get the effective group ID of the process."
+  (%getegid))
