36922213aa3347eaf4a451c5126e168513cd71bb
Flavio Cruz <flaviocruz@gmail.com>
2008-07-06 19:00:59 +0000

    Move back id types. Comments.

--- common/ids.lisp
+++ common/ids.lisp
@@ -1,6 +1,11 @@
 
-(defctype uid-t :int)
-(defctype gid-t :int)
+(in-package :hurd-common)
+
+(defctype uid-t :int "Represents an user ID")
+
+(defctype gid-t :int "Represents a group ID")
 
 (defun valid-id-p (id)
-  (not (= -1 id)))
+  "Checks if the ID (uid or gid) is valid."
+  (and (numberp id)
+       (>= id 0)))
