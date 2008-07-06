7eec85b792c0a971b20fd2cc84ed0f7f768f642e
Flavio Cruz <flaviocruz@gmail.com>
2008-07-06 18:59:28 +0000

    Comment code.

--- common/dirent.lisp
+++ common/dirent.lisp
@@ -1,12 +1,19 @@
 
+(in-package :hurd-common)
+
+;;
+;; File types for the 'd_type' field
+;; from struct dirent (bits/dirent.h).
+;; This enum is found at dirent.h.
+;;
 (defcenum dirent-type
-		  (:unknown 0)
-		  (:fifo 1)
-		  (:chr 2)
-		  (:dir 4)
-		  (:blk 6)
-		  (:reg 8)
-		  (:lnk 10)
-		  (:sock 12)
-		  (:wht 14))
+  (:unknown 0)
+  (:fifo 1)
+  (:chr 2)
+  (:dir 4)
+  (:blk 6)
+  (:reg 8)
+  (:lnk 10)
+  (:sock 12)
+  (:wht 14))
 
