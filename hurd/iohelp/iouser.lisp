a9914ce8655659a8fa82b0f147570e3d7fda423e
Flavio Cruz <flaviocruz@gmail.com>
2008-07-06 21:38:10 +0000

    Add comments. Add prefix % to private functions.

--- hurd/iohelp/iouser.lisp
+++ hurd/iohelp/iouser.lisp
@@ -1,53 +1,71 @@
 
+(in-package :hurd)
+
+;;
+;; This implements the IO user abstraction.
+;; An iouser is represented by a set of UID's and GID's.
+;;
+
 (defclass iouser ()
   ((uids :initform (list)
-	 :initarg :uids
-	 :accessor uids
-	 :documentation "List of uids")
+         :initarg :uids
+         :accessor uids
+         :documentation "List of uids")
    (gids :initform (list)
-	 :initarg :gids
-	 :reader gids
-	 :documentation "List of gids"))
+         :initarg :gids
+         :reader gids
+         :documentation "List of gids"))
   (:documentation "Groups list of uids and gids representing an I/O user"))
 
 (defun make-iouser (&key (uids nil) (gids nil) (old nil))
-  "Create an iouser based on lists or atoms of uids and gids"
+  "Create an iouser based on lists or atoms of uids and gids. Can also copy other iouser objects."
   (if old
-	(make-instance 'iouser
-				   :uids (uids old)
-				   :gids (gids old))
-	(make-instance 'iouser
-				   :uids (if (listp uids) uids (list uids))
-				   :gids (if (listp gids) gids (list gids)))))
+    ; Just make a copy.
+    (make-instance 'iouser
+                   :uids (uids old)
+                   :gids (gids old))
+    ; Use the indicated lists.
+    (make-instance 'iouser
+                   :uids (if (listp uids) uids (list uids))
+                   :gids (if (listp gids) gids (list gids)))))
 
 (defun make-iouser-mem (uids-ptr uids-len gids-ptr gids-len)
-  "Create an iouser based on foreign pointers"
+  "Create an iouser based on foreign pointers."
   (let ((obj (make-instance 'iouser
-			    :uids (idptr-to-list uids-ptr uids-len)
-			    :gids (idptr-to-list gids-ptr gids-len))))
-;    (free-ptr uids-ptr uids-len)
- ;   (free-ptr gids-ptr gids-len)
+                            :uids (%idptr-to-list uids-ptr uids-len)
+                            :gids (%idptr-to-list gids-ptr gids-len))))
+    ;(%free-ptr uids-ptr uids-len)
+    ;(%free-ptr gids-ptr gids-len)
     obj))
 
 (defmethod contains-uid ((iouser iouser) uid)
+  "Does it contain 'uid' in UIDs?"
   (member uid (uids iouser)))
 
 (defmethod contains-gid ((iouser iouser) gid)
+  "Does it contain 'gid' in GIDs?"
   (member gid (gids iouser)))
 
 (defmethod empty-uids-p ((iouser iouser))
+  "Is the UIDs set empty?"
   (null (uids iouser)))
 
 (defmethod empty-gids-p ((iouser iouser))
+  "Is the GID's set empty?"
   (null (gids iouser)))
 
 (defmethod restrict-iouser ((user1 iouser) (user2 iouser))
+  "Returns an intersection of user1 and user2, except when user1 is root and then we return user2."
   (cond
     ((contains-uid user1 0) user2) ; root user
     (t
       (make-iouser :uids (intersection (uids user1)
-				       (uids user2))
-		   :gids (intersection (gids user1)
-				       (gids user2))))))
+                                       (uids user2))
+                   :gids (intersection (gids user1)
+                                       (gids user2))))))
+
 (defmethod print-object ((iouser iouser) stream)
-  (format stream "#<iouser: uids -> ~s ; gids -> ~s>" (uids iouser) (gids iouser)))
+  "Print an iouser object."
+  (format stream
+          "#<iouser: uids -> ~s ; gids -> ~s>"
+          (uids iouser) (gids iouser)))
--- hurd/iohelp/utils.lisp
+++ hurd/iohelp/utils.lisp
@@ -1,11 +1,15 @@
 
-(defun idptr-to-list (ptr size)
-  (loop for i from 0 below size
-	collect (mem-aref ptr :unsigned-int i)))
+(in-package :hurd)
 
-(defun free-idptr (ptr size)
+(defun %idptr-to-list (ptr size)
+  "Transforms a foreign array 'ptr' with 'size' elements into a list."
+  (loop for i from 0 below size
+        collect (mem-aref ptr :unsigned-int i)))
+
+(defun %free-idptr (ptr size)
+  "Free's a foreign array with 'size' elements."
   (let ((ptr ptr))
     (loop for i from 0 below size
-	  do (progn
-	       (foreign-free ptr)
-	       (incf-pointer ptr)))))
+          do (progn
+               (foreign-free ptr)
+               (incf-pointer ptr)))))
