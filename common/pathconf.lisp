0c581c960073ef158590a298c88c1db3d3896206
Flavio Cruz <flaviocruz@gmail.com>
2008-07-06 19:01:57 +0000

    Comments.

--- common/pathconf.lisp
+++ common/pathconf.lisp
@@ -1,23 +1,29 @@
 
+(in-package :hurd-common)
+
+;;
+;; Values for the name argument to pathconf.
+;; These can be found at bits/confname.h
+;;
 (defcenum pathconf-type
-		  :link-max
-		  :max-canon
-		  :max-input
-		  :name-max
-		  :path-max
-		  :pipe-buf
-		  :chown-restricted
-		  :no-trunc
-		  :vdisable
-		  :sync-io
-		  :prio-io
-		  :sock-maxbuf
-		  :filesizebits
-		  :rec-incr-xfer-size
-		  :rec-max-xfer-size
-		  :rec-min-xfer-size
-		  :rec-xfer-align
-		  :alloc-size-min
-		  :symlink-max
-		  :2-symlinks)
+  :link-max
+  :max-canon
+  :max-input
+  :name-max
+  :path-max
+  :pipe-buf
+  :chown-restricted
+  :no-trunc
+  :vdisable
+  :sync-io
+  :prio-io
+  :sock-maxbuf
+  :filesizebits
+  :rec-incr-xfer-size
+  :rec-max-xfer-size
+  :rec-min-xfer-size
+  :rec-xfer-align
+  :alloc-size-min
+  :symlink-max
+  :2-symlinks)
 
