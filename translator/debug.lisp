1f77471514c3ccb29051b8694b29d07c3edbd23f
Flavio Cruz <flaviocruz@gmail.com>
2008-07-07 00:24:59 +0000

    Add more comments.

--- translator/debug.lisp
+++ translator/debug.lisp
@@ -1,12 +1,15 @@
 
+(in-package :hurd-translator)
+
 (defun show-table (fun module)
+  "Shows a routine table from the module 'module' using 'fun' to show it."
   (let ((module-name (string-downcase (string module))))
 	(format t "Table for ~a routines:~%" module-name)
 	(funcall fun)
 	(format t "~%")))
 
-;; display interface routines table at the C level
 (defun show-tables ()
+  "Display interface routines table at the C level."
   (show-table #'%get-io-info 'io)
   (show-table #'%get-fs-info 'fs)
   (show-table #'%get-fsys-info 'fsys))
