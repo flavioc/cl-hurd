5e9f58c17c0fced2bd399d6364c8818ac1bbd513
Flavio Cruz <flaviocruz@gmail.com>
2008-07-07 00:25:38 +0000

    Add more comments. Change port-exists to port-exists-p.

--- translator/utils.lisp
+++ translator/utils.lisp
@@ -1,12 +1,16 @@
 
+(in-package :hurd-translator)
+
 (defmethod propagate-read-to-execute ((stat stat))
-  (if (has-perms stat 'read 'owner)
-	(set-perms stat 'exec 'owner))
-  (if (has-perms stat 'read 'group)
-	(set-perms stat 'exec 'group))
-  (if (has-perms stat 'read 'others)
-	(set-perms stat 'exec 'others))
+  "Enables the execute permission bit if the read bit is on."
+  (if (has-perms-p stat 'read 'owner)
+    (set-perms stat 'exec 'owner))
+  (if (has-perms-p stat 'read 'group)
+    (set-perms stat 'exec 'group))
+  (if (has-perms-p stat 'read 'others)
+    (set-perms stat 'exec 'others))
   t)
 
-(defun port-exists (port)
+(defun port-exists-p (port)
+  "Checks port existence on the actual translator."
   (has-port (port-bucket *translator*) port))
