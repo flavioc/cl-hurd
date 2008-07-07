3dcee5cd68fe9c94e976bcac4419a03efde79c84
Flavio Cruz <flaviocruz@gmail.com>
2008-07-07 16:26:06 +0000

    Add vm-deallocate.

--- mach/vm-allocate.lisp
+++ mach/vm-allocate.lisp
@@ -11,3 +11,13 @@
 (defun vm-allocate (address size anywhere &optional (task (task-self)))
   "Allocate a region of virtual memory."
   (%vm-allocate task address size anywhere))
+
+(defcfun ("vm_deallocate" %vm-deallocate)
+  err
+  (task vm-task)
+  (address vm-address)
+  (size vm-size))
+
+(defun vm-deallocate (address size &optional (task (task-self)))
+  "Relinquishes access to a region of a task's address space, causing further access to that memory to fail."
+  (%vm-deallocate task address size))
