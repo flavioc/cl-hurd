caeaa1a918991baa2b3447a0ab3dd2daf449a5e3
Flavio Cruz <flaviocruz@gmail.com>
2008-07-07 00:27:03 +0000

    Add comments. Remove file-read declaration.

--- tree-translator/class.lisp
+++ tree-translator/class.lisp
@@ -1,30 +1,44 @@
 
-(defclass tree-translator (translator)
-  ((name :initform "tree-translator")))
+(in-package :hurd-tree-translator)
 
+;;
+;; This file a special kind of translator:
+;; the tree-translator
+;; It exposes without much work the directory structure.
+;;
+
+(defclass tree-translator (translator)
+  ((name :initform "tree-translator"
+         :documentation "Translator name"))
+  (:documentation "The tree-translator."))
+
+;; It ensures the root node is a directory
+;; and calls the function fill-root-node to fill the directory
+;; structure.
 (define-callback make-root-node tree-translator
-				 (underlying-stat)
-   (when (not (is-dir-p underlying-stat))
-	 (propagate-read-to-execute underlying-stat))
-   (set-type underlying-stat 'dir)
-   (set-trans underlying-stat nil)
-   (let ((obj (make-dir "root" underlying-stat)))
-	 (fill-root-node translator obj)
-	 obj))
+                 (underlying-stat)
+  (when (not (is-dir-p underlying-stat))
+    (propagate-read-to-execute underlying-stat))
+  (set-type underlying-stat 'dir)
+  (set-trans underlying-stat nil)
+  (let ((obj (make-dir "root" underlying-stat)))
+    (fill-root-node translator obj)
+    obj))
 
 (defmethod fill-root-node ((translator tree-translator) (root dir-entry))
+  "This should be used to construct the directory structure. 'root' is the newly created
+root node."
   nil)
 
 (define-callback dir-lookup tree-translator
 				 (node user filename)
-  (warn "dir-look ~s in ~s" filename (name node))
   (cond
-	((string= filename ".")
-	 node)
-	((string= filename "..")
-	 (parent node))
-	(t
-	  (get-entry node filename))))
+    ((string= filename ".")
+     node)
+    ((string= filename "..")
+     (parent node))
+    (t
+      (get-entry node filename))))
 
 (define-callback number-of-entries tree-translator
 				 (node user)
@@ -33,21 +47,17 @@
 (define-callback get-entries tree-translator
 				(node user start end)
   (let* ((all (loop for value being the hash-values of (entries node)
-				   using (hash-key key)
-				   collect (make-dirent key value)))
-		 (sorted (sort all (lambda (a b) (string< (name a) (name b))))))
-	(subseq sorted start (1+ end))))
+                    using (hash-key key)
+                    collect (make-dirent key value)))
+         (sorted (sort all (lambda (a b) (string< (name a) (name b))))))
+    (subseq sorted start (1+ end))))
 
 (define-callback create-directory tree-translator
 				 (node user name mode)
   (let ((new-stat (make-stat (stat node))))
-	(setf (stat-get new-stat 'mode) mode)
-	(add-entry node (make-dir name new-stat node))))
+    (setf (stat-get new-stat 'mode) mode)
+    (add-entry node (make-dir name new-stat node))))
 
 (define-callback remove-entry tree-translator
 				 (node user name directory-p)
   (remove-dir-entry node name))
-
-(define-callback file-read tree-translator
-				 (node user start amount stream)
-				 nil)
