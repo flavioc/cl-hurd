8703edd3791d07e216d0d637e776b20960df3e34
Flavio Cruz <flaviocruz@gmail.com>
2008-07-07 17:14:39 +0000

    Remove functions.

--- mach/functions.lisp
+++ mach/functions.lisp
@@ -6,22 +6,6 @@
   (and (numberp p)
        (> p 0)))
 
-
-(defcfun ("mach_port_mod_refs" %mach-port-mod-refs)
-	 err
-	 (task ipc-space)
-	 (port-name port)
-	 (right port-right)
-	 (delta port-delta))
-
-(defun port-mod-refs (port right
-                           &optional
-                           (delta -1) ; drop 1 ref by default
-                           (task (task-self)))
-  "Modify the specified port right's count of user references."
-  (%mach-port-mod-refs task port right delta))
-
-
 (defcfun ("mach_port_move_member" %mach-port-move-member)
   err
   (task ipc-space)
@@ -32,22 +16,6 @@
                                 &optional (task (task-self)))
   "Move the specified receive right into or out of the specified port set."
   (%mach-port-move-member task before after))
-
-
-(defcfun ("mach_port_insert_right" %mach-port-insert-right)
-  err
-  (task ipc-space)
-  (port-name port)
-  (right port)
-  (right-type msg-type-name))
-
-(defun port-insert-right (port right right-type
-			       &optional (task (task-self)))
-  "Insert the specified port right into the target task."
-  (%mach-port-insert-right task
-                           port
-                           right
-                           right-type))
 
 
 (defcfun ("mach_msg_server_timeout" %mach-msg-server-timeout)
