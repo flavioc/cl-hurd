49ec84ef79a3cc4b311439f1c8bd913c2ec71683
Flavio Cruz <flaviocruz@gmail.com>
2008-07-07 22:41:37 +0000

    Evaluate timeout before to properly set msg-server options.

--- mach/msg-server.lisp
+++ mach/msg-server.lisp
@@ -9,19 +9,18 @@
   (options msg-option)
   (timeout msg-timeout))
 
-(defmacro msg-server-timeout (demuxer port-set &optional (timeout 0) (max-size 0))
+(defmacro msg-server-timeout (demuxer port-set &optional timeout max-size)
   "Receive RPC request messages on port-set and pass them to function demuxer with a timeout."
-  (with-gensyms (callback-name)
+  (with-gensyms (callback-name timeout-val)
     `(progn
        (defcallback ,callback-name :boolean ((in :pointer) (out :pointer))
          (funcall ,demuxer in out))
-       (%mach-msg-server-timeout (callback ,callback-name)
-                                 ,max-size
-                                 ,port-set
-                                 ,(if (> timeout 0)
-                                    '(:rcv-timeout)
-                                    '())
-                                 ,timeout))))
+       (let ((,timeout-val ,(if (null timeout) 0 timeout)))
+         (%mach-msg-server-timeout (callback ,callback-name)
+                                   ,(if (null max-size) 0 max-size)
+                                   ,port-set
+                                   (if (plusp ,timeout-val) '(:rcv-timeout) '())
+                                   ,timeout-val)))))
 
 (defcfun ("mach_msg_server" %mach-msg-server)
   err
@@ -29,11 +28,15 @@
   (max-size msg-size)
   (rcv-name port))
 
-(defmacro msg-server (demuxer port-set &optional (max-size 0))
+(defmacro msg-server (demuxer port-set &optional max-size)
   "Receive RPC request messages on port-set and pass them to function demuxer."
   (with-gensyms (callback-name)
     `(progn
        (defcallback ,callback-name :boolean ((in :pointer) (out :pointer))
          (funcall ,demuxer in out))
-       (%mach-msg-server (callback ,callback-name) ,max-size ,port-set))))
+       (%mach-msg-server (callback ,callback-name)
+                         ,(if (null max-size)
+                           0
+                           max-size)
+                         ,port-set))))
 
