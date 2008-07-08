6fbf3269a1716c5ed1ada0793ada105227ad378f
Flavio Cruz <flaviocruz@gmail.com>
2008-07-08 19:54:32 +0000

    Now with io-read support.

--- examples/zip.lisp
+++ examples/zip.lisp
@@ -13,23 +13,31 @@
          :documentation "Translator name"))
   (:documentation "Zip translator."))
 
-
 (defclass zip-entry (entry)
-  ((zip-entry :initarg :zip
-              :accessor zip-entry
-              :documentation "The zip-entry associated with this node."))
+  ((data-sequence :initarg :data
+                  :accessor data
+                  :documentation "The zip data associated with this file."))
   (:documentation "Extends entry with a zip-entry."))
 
 (define-callback allow-open zip-translator (node user flags is-new-p) t)
 
-;; XXX
 (define-callback file-read zip-translator
 				 (node user start amount stream)
-  (if (> start (stat-get (stat node) 'size))
-    nil
-    (progn
-      (write-sequence (string-to-octets "ola") stream)
-      t)))
+  (let* ((size (stat-get (stat node) 'size))
+         (size-res (- size start)))
+    (cond
+      ((not (plusp size-res))
+       nil)
+      (t
+        (let* ((total (if (null amount)
+                        size-res
+                        (min size-res amount)))
+               (end (+ start total)))
+        (write-sequence (subseq (data node) start end)
+                        stream)
+        (if (eq end size)
+          (write-byte #xA0 stream))
+        t)))))
 
 ;; XXX
 (define-callback file-write zip-translator
@@ -38,14 +46,23 @@
 
 (defun %create-zip-file (name parent entry)
   "Create a new zip entry."
-  (let ((obj (make-instance 'zip-entry
-                            :stat (make-stat (stat parent)
-                                             :size (zipfile-entry-size entry))
-                            :name name
-                            :parent parent
-                            :zip entry)))
-    (setup-entry obj)
-    obj))
+  (let ((data-stream
+          (make-in-memory-output-stream
+            :element-type '(unsigned-byte 8)))
+        seq
+        stat)
+    (zipfile-entry-contents entry data-stream)
+    (setf seq (get-output-stream-sequence data-stream))
+    (setf stat (make-stat (stat parent)
+                          :size (length seq)))
+    (clear-perms stat 'exec) ; Clear exec permissions.
+    (let ((obj (make-instance 'zip-entry
+                              :stat stat
+                              :name name
+                              :parent parent
+                              :data seq)))
+      (setup-entry obj)
+      obj)))
 
 (defun %create-zip-dir (name parent)
   "Create a new zip directory."
@@ -57,6 +74,7 @@
          (this-name (first name))
          (final-p (null name-rest)))
     (if (string= this-name "")
+      ;; Last node was a directory and it is already created.
       (return-from add-zip-file nil))
     (multiple-value-bind (entry found-p)
       (get-entry node this-name)
@@ -77,7 +95,8 @@
   (do-zipfile-entries (name entry *zip*)
                       (add-zip-file node (split-path name) entry)))
 
-(defvar *zip-translator* (make-translator 'zip-translator) "The translator.")
+(defvar *zip-translator*
+  (make-translator 'zip-translator) "The translator.")
 
 ;; Startup the translator.
 (run-translator *zip-translator*)
@@ -87,22 +106,23 @@
 (define-callback file-utimes zip-translator
 				 (node user atime mtime)
   (when atime
-	(setf (stat-get (stat node) 'atime) atime))
+    (setf (stat-get (stat node) 'atime) atime))
   (when mtime
-	(setf (stat-get (stat node) 'mtime) mtime))
+    (setf (stat-get (stat node) 'mtime) mtime))
   t)
 
 (define-callback file-chmod zip-translator
 				 (node user mode)
-  (set-perms-if (stat node) 'read 'others
-				(has-perms-p mode 'read 'others)))
+  (set-perms-if (stat node)
+                (has-perms-p mode 'read 'others)
+                'read 'others))
 
 (define-callback file-chown zip-translator
 				 (node user uid gid)
   (when (valid-id-p uid)
-	(setf (stat-get (stat node) 'uid) uid))
+    (setf (stat-get (stat node) 'uid) uid))
   (when (valid-id-p gid)
-	(setf (stat-get (stat node) 'gid) gid))
+    (setf (stat-get (stat node) 'gid) gid))
   t)
 
 (define-callback allow-author-change zip-translator
