fcf6c8de0b61639e42b9f7b990938ad2a44fa7e8
Flavio Cruz <flaviocruz@gmail.com>
2008-07-10 19:10:30 +0000

    Add file-check-access callback.

---
+++ translator/interfaces/file-check-access.lisp
@@ -0,0 +1,12 @@
+
+(in-package :hurd-translator)
+
+(def-fs-interface :file-check-access ((file port)
+                                      (flags :pointer))
+  (with-lookup protid file
+    (setf (mem-ref flags 'open-flags-t)
+          (report-access *translator*
+                         (get-node protid)
+                         (get-user protid)))
+      t))
+
