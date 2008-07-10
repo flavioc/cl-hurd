d31bf50b2f94355428900b1852db023e509b5b83
Flavio Cruz <flaviocruz@gmail.com>
2008-07-10 19:12:20 +0000

    Change MIG to MIG_SERVER. Add MIG_USER.

--- defs.mk
+++ defs.mk
@@ -4,4 +4,6 @@ LDFLAGS = -lmachuser -lc -lhurduser
 
 HURD_DIRECTORY = /usr/include/hurd
 MACH_DIRECTORY = /usr/include/mach
-MIG = mig -v -prefix lisp_ -user /dev/null -header /dev/null -server
+MIG_SERVER = mig -v -prefix lisp_ -user /dev/null -header /dev/null -server
+MIG_USER = mig -v -server /dev/null -header /dev/null -user
+
