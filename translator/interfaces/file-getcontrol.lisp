ccbb48e86c24c475d9d478f570e7c135e9f09fe8
Flavio Cruz <flaviocruz@gmail.com>
2008-07-10 19:10:16 +0000

    Add file-getcontrol callback.

---
+++ translator/interfaces/file-getcontrol.lisp
@@ -0,0 +1,17 @@
+
+(in-package :hurd-translator)
+
+;; Return control port for this filesystem.
+(def-fs-interface :file-getcontrol ((file port)
+                                    (control port-pointer)
+                                    (control-type :pointer))
+  (with-lookup protid file
+    (block file-getcontrol
+           (unless (is-controller-p (get-node protid)
+                                    (get-user protid))
+             (return-from file-getcontrol :not-permitted))
+           (let ((port (add-control-port (port-bucket *translator*))))
+             (setf (mem-ref control 'port) (get-right port))
+             (setf (mem-ref control-type 'msg-type-name) :make-send)
+             t))))
+
