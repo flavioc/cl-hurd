e6592fbdbe436f4dd1bc6e0fb7283b2f976aee9f
Flavio Cruz <flaviocruz@gmail.com>
2008-07-10 19:09:35 +0000

    Add fsys-syncfs callback.

---
+++ translator/interfaces/fsys-syncfs.lisp
@@ -0,0 +1,17 @@
+
+(in-package :hurd-translator)
+
+;; Sync a filesystem.
+(def-fsys-interface :fsys-syncfs ((control port)
+                                  (reply port)
+                                  (reply-type msg-type-name)
+                                  (wait :boolean)
+                                  (children :boolean))
+  (when (port-exists-p control)
+    (let ((root-user (make-iouser-root)))
+      (file-syncfs *translator*
+                   root-user
+                   wait
+                   children)
+      t)))
+
