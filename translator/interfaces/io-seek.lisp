70931fa5d0588b8befc30af7869c1782cb5cf871
Flavio Cruz <flaviocruz@gmail.com>
2008-07-11 15:34:08 +0000

    Add io-seek callback.

---
+++ translator/interfaces/io-seek.lisp
@@ -0,0 +1,21 @@
+
+(in-package :hurd-translator)
+
+(def-io-interface :io-seek ((io port)
+                            (offset loff-t)
+                            (whence seek-type)
+                            (newoffset :pointer))
+  (with-lookup protid io
+    (case whence
+      (:seek-cur
+        (incf offset (file-offset (open-node protid))))
+      (:seek-end
+        (incf offset (stat-get (stat (get-node protid)) 'size))))
+    (cond
+      ((plusp offset)
+       (setf (mem-ref newoffset 'loff-t) offset
+             (file-offset (open-node protid)) offset)
+       t)
+      (t
+        :invalid-argument))))
+
