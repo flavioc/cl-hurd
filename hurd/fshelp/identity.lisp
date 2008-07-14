e309978abb4d3e05e821c9b9f08822e975fc9278
Flavio Cruz <flaviocruz@gmail.com>
2008-07-14 22:40:53 +0000

    Add get-io-identity for io identities (io-identity callback).

---
+++ hurd/fshelp/identity.lisp
@@ -0,0 +1,22 @@
+
+(in-package :hurd)
+
+(defclass identity-spec (port-info)
+  ((ino :initform nil
+        :initarg :ino
+        :accessor ino
+        :documentation "Ino stat's field."))
+  (:documentation "IO identification port."))
+
+(defmethod get-io-identity ((bucket port-bucket) ino)
+  "Return an identity port  for the node numbered 'ino'."
+  (let ((found (bucket-find bucket
+                            (lambda (port)
+                              (and (typep port 'identity-spec)
+                                   (eq (ino port) ino))))))
+    (unless found
+      (let ((new-port (make-instance 'identity-spec :ino ino)))
+        (add-port bucket new-port)
+        (setf found new-port)))
+    (get-right found)))
+
