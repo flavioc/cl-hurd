9ba1f9f999c2e07d1513b76f5e3f1025b7e419b7
Flavio Cruz <flaviocruz@gmail.com>
2008-07-14 12:56:00 +0000

    Add dir-rename callback.

---
+++ translator/interfaces/dir-rename.lisp
@@ -0,0 +1,32 @@
+
+(in-package :hurd-translator)
+
+(def-fs-interface :dir-rename ((old-dir port)
+                               (old-name :string)
+                               (new-dir port)
+                               (new-name :string)
+                               (excl :boolean))
+  (with-lookup old-dir-protid old-dir
+    (block dir-rename
+           (unless (port-exists-p new-dir)
+             (return-from dir-rename :invalid-cross-device-link))
+           (with-lookup new-dir-protid new-dir
+             (let ((found-old-node (dir-lookup *translator*
+                                               (get-node old-dir-protid)
+                                               (get-user old-dir-protid)
+                                               old-name)))
+               (unless found-old-node
+                 (return-from dir-rename :no-such-file))
+               (let ((found-new-node (dir-lookup *translator*
+                                                 (get-node new-dir-protid)
+                                                 (get-user new-dir-protid)
+                                                 new-name)))
+                 (when (and found-new-node excl)
+                   (return-from dir-rename :file-exists))
+                 (file-rename *translator*
+                              (get-user old-dir-protid)
+                              (get-node old-dir-protid)
+                              old-name
+                              (get-node new-dir-protid)
+                              new-name)))))))
+
