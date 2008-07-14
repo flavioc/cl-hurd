9430ad4590f7432cc01637e25c787555e3937243
Flavio Cruz <flaviocruz@gmail.com>
2008-07-14 22:47:28 +0000

    Add io-identity.

---
+++ translator/interfaces/io-identity.lisp
@@ -0,0 +1,20 @@
+
+(in-package :hurd-translator)
+
+(def-io-interface :io-identity ((file port)
+                                (id port-pointer)
+                                (id-type :pointer)
+                                (fsys port-pointer)
+                                (fsys-type :pointer)
+                                (fileno :pointer))
+  (with-lookup protid file
+    (let* ((ino (stat-get (get-stat protid) 'ino))
+           (io-identity (get-io-identity (port-bucket *translator*) ino)))
+      (warn "identity port ~s ino ~s io-identity ~s" (identity-port *translator*) ino io-identity)
+      (setf (mem-ref id 'port) io-identity
+            (mem-ref id-type 'msg-type-name) :make-send
+            (mem-ref fsys 'port) (identity-port *translator*)
+            (mem-ref fsys-type 'msg-type-name) :make-send
+            (mem-ref fileno 'ino-t) ino)
+      t)))
+
