7b13dd9c9a18eed515374990dce1a314501a05f9
Flavio Cruz <flaviocruz@gmail.com>
2008-07-14 22:45:57 +0000

    Use running-p. Rename run to inner-run.

--- translator/run.lisp
+++ translator/run.lisp
@@ -1,18 +1,19 @@
 
 (in-package :hurd-translator)
 
-(defmethod run ((translator translator))
+(defmethod inner-run ((translator translator))
   "Run the translator server."
   (let ((*translator* translator))
-    (run-server (lambda (port in out) (translator-demuxer in out))
+    (run-server (lambda (port in out)
+                  (translator-demuxer in out))
                 (port-bucket *translator*))))
 
-(defun run-translator (translator)
+(defmethod run-translator ((translator translator))
   "Setup the translator and the run it."
-  (when (setup translator)
+  (when (running-p translator)
     (let* ((under (underlying-node translator))
            (stat (io-stat under)))
       (setf (root translator)
             (make-root-node translator (make-stat stat))))
-    (run translator)))
+    (inner-run translator)))
 
