0028a03a121a244b6f82e27638b161d31763beff
Flavio Cruz <flaviocruz@gmail.com>
2008-07-14 22:39:43 +0000

    Make stat-set accept :now for time arguments. Use *mapped-time* for that.

--- common/stat.lisp
+++ common/stat.lisp
@@ -82,34 +82,6 @@ uid, gid, size, atim, mtim, ctim, blksize, blocks, author, flags."
       (otherwise
         (foreign-slot-value ptr 'stat-struct what)))))
 
-(defcstruct time-value-struct
-  "Time value struct returned by the Mach kernel.
-Definition can be found at mach/time_value.h"
-  (seconds :int)
-  (microseconds :int))
-
-(define-foreign-type time-value-type ()
-  ()
-  (:documentation "CFFI type for thye time-value-struct.")
-  (:actual-type :pointer)
-  (:simple-parser time-value-t))
-
-(defclass time-value ()
-  ((ptr :reader ptr
-        :initarg :ptr
-        :documentation "Pointer to a time-value struct."))
-  (:documentation "Time-value class for objects that allocate pointers to time-value structures."))
-
-(defmethod translate-from-foreign (value (type time-value-type))
-  "Translate a time-value pointer to a time-value object."
-  (if (= -1 (foreign-slot-value value 'time-value-struct 'microseconds))
-    nil
-    (make-instance 'time-value :ptr value)))
-
-(defmethod translate-to-foreign (value (type time-value-type))
-  "Translate a time-value object to a foreign time-value pointer."
-  (ptr value))
-
 (defun %stat-time-set (ptr field new-value)
   "From a stat pointer 'ptr' set the timespec field 'field' to 'new-value'."
   (let ((timespec (foreign-slot-value ptr 'stat-struct field))) ; Get the field
@@ -118,12 +90,19 @@ Definition can be found at mach/time_value.h"
        ; Copy the time-value seconds
        ; and convert the microseconds to nanoseconds.
        (setf (foreign-slot-value timespec 'timespec-struct 'sec)
-             (foreign-slot-value (ptr new-value) 'time-value-struct 'seconds))
-       (setf (foreign-slot-value timespec 'timespec-struct 'nsec)
+             (foreign-slot-value (ptr new-value) 'time-value-struct 'seconds)
+             (foreign-slot-value timespec 'timespec-struct 'nsec)
              (* 1000
                 (foreign-slot-value (ptr new-value)
                                     'time-value-struct 'microseconds)))
        t)
+      ((eq new-value :now)
+       ; Use *mapped-time* to update fields.
+       (setf (foreign-slot-value timespec 'timespec-struct 'sec)
+             (maptime-seconds *mapped-time*)
+             (foreign-slot-value timespec 'timespec-struct 'nsec)
+             (* 1000
+                (maptime-microseconds *mapped-time*))))
       (t
         ; For everything else just copy the value to seconds.
         (setf (foreign-slot-value timespec 'timespec-struct 'sec)
