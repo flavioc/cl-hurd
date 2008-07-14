ec17f424a86bd458931fb6fa9e88a86715748a2f
Flavio Cruz <flaviocruz@gmail.com>
2008-07-14 22:46:38 +0000

    Align code.

--- translator/interfaces/file-utimes.lisp
+++ translator/interfaces/file-utimes.lisp
@@ -2,8 +2,8 @@
 (in-package :hurd-translator)
 
 (def-fs-interface :file-utimes ((file port)
-								(atimein time-value-t)
-								(mtimein time-value-t))
+                                (atimein time-value-t)
+                                (mtimein time-value-t))
   (with-lookup protid file
     (file-utimes *translator*
                  (get-node protid)
