f829b5b1e17eea5de0403ce8d250d2ce272da141
Flavio Cruz <flaviocruz@gmail.com>
2008-07-14 22:47:02 +0000

    Add io-restrict-auth.

---
+++ translator/interfaces/io-restrict-auth.lisp
@@ -0,0 +1,22 @@
+
+(in-package :hurd-translator)
+
+(def-io-interface :io-restrict-auth ((file port)
+                                     (new-port port-pointer)
+                                     (new-port-type :pointer)
+                                     (uids :pointer)
+                                     (nuids msg-type-number)
+                                     (gids :pointer)
+                                     (ngids msg-type-number))
+  (with-lookup protid file
+    (let* ((new-user (make-iouser-mem uids nuids gids ngids))
+           (restricted-user (restrict-iouser (get-user protid)
+                                             new-user))
+           (new-protid (new-protid *translator*
+                                   restricted-user
+                                   (open-node protid))))
+      (warn "new user ~s" restricted-user)
+      (setf (mem-ref new-port 'port) (get-right new-protid)
+            (mem-ref new-port-type 'msg-type-name) :make-send)
+      t)))
+      
