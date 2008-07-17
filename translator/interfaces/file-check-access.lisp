3455e801b2f5d2b3e0dafc0dff17b253385245b2
Flavio Cruz <flaviocruz@gmail.com>
2008-07-17 21:49:19 +0000

    Use open-flags, not open-flags-t.

--- translator/interfaces/file-check-access.lisp
+++ translator/interfaces/file-check-access.lisp
@@ -4,7 +4,7 @@
 (def-fs-interface :file-check-access ((file port)
                                       (flags :pointer))
   (with-lookup protid file
-    (setf (mem-ref flags 'open-flags-t)
+    (setf (mem-ref flags 'open-flags)
           (report-access *translator*
                          (get-node protid)
                          (get-user protid)))
