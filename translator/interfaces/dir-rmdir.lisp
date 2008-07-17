eda5f6f5ba4734ca8e73cd5e80b8b1b520a9829b
Flavio Cruz <flaviocruz@gmail.com>
2008-07-17 21:50:54 +0000

    Rename remove-entry to remove-directory entry.

--- translator/interfaces/dir-rmdir.lisp
+++ translator/interfaces/dir-rmdir.lisp
@@ -4,10 +4,10 @@
 (def-fs-interface :dir-rmdir ((port port)
 							  (name :string))
   (with-lookup protid port
-    (if (remove-entry *translator*
-                      (get-node protid)
-                      (get-user protid)
-                      name
-                      t)
+    (if (remove-directory-entry *translator*
+                                (get-node protid)
+                                (get-user protid)
+                                name
+                                t)
       t
       :not-permitted)))
