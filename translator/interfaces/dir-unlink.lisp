9c28baf77381f26e951f79567365111b677229f7
Flavio Cruz <flaviocruz@gmail.com>
2008-07-17 21:50:26 +0000

    Rename remove-entry to remove-directory entry.

--- translator/interfaces/dir-unlink.lisp
+++ translator/interfaces/dir-unlink.lisp
@@ -4,10 +4,10 @@
 (def-fs-interface :dir-unlink ((port port)
 							   (name :string))
   (with-lookup protid port
-    (if (remove-entry *translator*
-                      (get-node protid)
-                      (get-user protid)
-                      name
-                      nil) ; Not a directory
+    (if (remove-directory-entry *translator*
+                                (get-node protid)
+                                (get-user protid)
+                                name
+                                nil) ; Not a directory
       t
       :not-permitted)))
