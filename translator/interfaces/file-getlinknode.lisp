132d96f1ab2870f7f4fd48ffd1f14b02a2feb1ea
Flavio Cruz <flaviocruz@gmail.com>
2008-07-17 21:50:07 +0000

    Fix logic error.

--- translator/interfaces/file-getlinknode.lisp
+++ translator/interfaces/file-getlinknode.lisp
@@ -6,7 +6,7 @@
 									 (ret-type :pointer))
   (with-lookup protid port
     (block file-getlinknode
-           (unless (eq (root *translator*) (get-node protid))
+           (when (eq (root *translator*) (get-node protid))
              (return-from file-getlinknode :resource-busy))
            (setf (mem-ref ret-port 'port) (get-right protid))
            (setf (mem-ref ret-type 'msg-type-name) :make-send)
