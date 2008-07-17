aded990eeb33f3c8bfa2f3eef3520b06294f56ca
Flavio Cruz <flaviocruz@gmail.com>
2008-07-17 21:48:07 +0000

    Add dir-link callback.

---
+++ translator/interfaces/dir-link.lisp
@@ -0,0 +1,33 @@
+
+(in-package :hurd-translator)
+
+(def-fs-interface :dir-link ((dir port)
+                             (file port)
+                             (name :string)
+                             (excl :boolean))
+  (block dir-link
+         (with-lookup dir-protid dir
+           (unless (port-exists-p file)
+             ; Only nodes in this translator!
+             (return-from dir-link :invalid-cross-device-link))
+           (with-lookup file-protid file
+             (let ((found-node (dir-lookup *translator*
+                                           (get-node dir-protid)
+                                           (get-user dir-protid)
+                                           name)))
+               (when (and found-node excl)
+                 (return-from dir-link :file-exists))
+               (let ((ret-code (create-hard-link *translator*
+                                                 (get-node dir-protid)
+                                                 (get-user dir-protid)
+                                                 (get-node file-protid)
+                                                 name)))
+                 (cond
+                   ((eq ret-code t)
+                     (deallocate-send-right file-protid)
+                     t)
+                   ((eq ret-code nil)
+                    nil)
+                   (t
+                     ret-code))))))))
+
