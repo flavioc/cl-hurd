0441545c67a3ceaf9dd090b0718f7500457745a4
Flavio Cruz <flaviocruz@gmail.com>
2008-07-17 21:46:37 +0000

    Use new open-flags semantic.

--- translator/interfaces/io-get-openmodes.lisp
+++ translator/interfaces/io-get-openmodes.lisp
@@ -4,6 +4,6 @@
 (def-io-interface :io-get-openmodes ((port port)
 									 (bits :pointer))
   (with-lookup protid port
-    (setf (mem-ref bits 'open-flags-t) (flags (open-node protid)))
+    (setf (mem-ref bits 'open-flags) (flags (open-node protid)))
     t))
 
