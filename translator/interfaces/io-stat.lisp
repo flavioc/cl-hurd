8274ce0599acf855ac37d5f367142edc5c0dc598
Flavio Cruz <flaviocruz@gmail.com>
2008-07-19 18:13:36 +0100

    Indent. Remove set-active-trans call. Comment message.

--- translator/interfaces/io-stat.lisp
+++ translator/interfaces/io-stat.lisp
@@ -4,9 +4,9 @@
 (def-io-interface :io-stat ((io port)
 							(stat-info stat-t))
   (with-lookup protid io
-	  (stat-copy stat-info (get-stat protid))
+    ;(warn "stating node ~s" (get-node protid))
+    (stat-copy stat-info (get-stat protid))
     (set-root stat-info nil)
-    (set-active-trans stat-info nil)
     (set-active-trans stat-info
                       (box-translated-p (get-box protid)))
     (set-root stat-info
