556722e37a8379d61cbec026281c5d8c77d45620
Flavio Cruz <flaviocruz@gmail.com>
2008-07-20 16:39:49 +0100

    Simplify and use list-to-foreign-string-zero-separated.

--- translator/interfaces/options.lisp
+++ translator/interfaces/options.lisp
@@ -6,27 +6,19 @@
     (let* ((args0 (get-options *translator*))
            (args (cons
                    (name *translator*) args0))
-           (len-args (mapcar (lambda (str) (1+ (length str)))
-                             args))
-           (total (reduce #'+ len-args)))
+           (len-args (string-list-len args))
+           (total (sum-list len-args)))
       (when (< (mem-ref data-len 'msg-type-number)
                total)
-        (let ((new-ptr (mmap (make-pointer 0)
-                             total
-                             '(:prot-read :prot-write)
-                             '(:map-anon)
-                             0
-                             0)))
-          (when (null new-ptr)
-            (return-from %get-options-callback :no-memory))
-          (setf (mem-ref data :pointer) new-ptr)))
+          (setf (mem-ref data :pointer)
+                (mmap (make-pointer 0)
+                      total
+                      '(:prot-read :prot-write)
+                      '(:map-anon)
+                      0
+                      0)))
       (setf (mem-ref data-len 'msg-type-number) total)
-      (let ((ptr (mem-ref data :pointer)))
-        (loop for item in args
-              for item-len in len-args
-              do (progn
-                   (lisp-string-to-foreign item
-                                           ptr
-                                           item-len)
-                   (incf-pointer ptr item-len))))
+      (list-to-foreign-string-zero-separated args
+                                             (mem-ref data :pointer)
+                                             len-args)
       t)))
