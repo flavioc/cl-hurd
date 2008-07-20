9a2c56d8c62e23e7d34b78fdfefacc03a9106144
Flavio Cruz <flaviocruz@gmail.com>
2008-07-20 16:35:43 +0100

    Indent.

--- translator/demuxer.lisp
+++ translator/demuxer.lisp
@@ -3,5 +3,9 @@
 
 (defun translator-demuxer (in out)
   "The translator demuxer."
-  (stack-servers in out io-server notify-server fs-server fsys-server))
+  (stack-servers in out
+                 io-server
+                 notify-server
+                 fs-server
+                 fsys-server))
 
