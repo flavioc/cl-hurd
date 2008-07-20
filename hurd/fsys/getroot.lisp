fa7ebf52e7b4cf368eb8d29278220b58d05bc4ce
Flavio Cruz <flaviocruz@gmail.com>
2008-07-20 21:15:22 +0100

    Make fsys-getroot more lispy, returning multiple values and accepting a iouser.

--- hurd/fsys/getroot.lisp
+++ hurd/fsys/getroot.lisp
@@ -10,22 +10,29 @@
   (uids-count msg-type-number)
   (gids :pointer)
   (gids-count msg-type-number)
-  (flags :int)
+  (flags open-flags)
   (do-retry :pointer)
-  (retry-name :string)
+  (retry-name :pointer)
   (file port-pointer))
 
-(defun fsys-getroot (control dotdot dotdot-poly
-                             uids uids-count
-                             gids gids-count
-                             flags
-                             do-retry retry-name)
-  "Returns a file to tyhe root of the filesystem. See hurd/fsys.defs for details."
-  (with-foreign-pointer (control (foreign-type-size 'pointer))
-    (let ((return-code (%fsys-getroot control dotdot
-                                      dotdot-poly
-                                      uids uids-count
-                                      gids gids-count
-                                      flags do-retry
-                                      retry-name control)))
-      (select-error return-code (mem-ref control 'port)))))
+(defun fsys-getroot (control dotdot dotdot-poly user flags)
+  "Returns a file to the root of the filesystem. See hurd/fsys.defs for details."
+  (with-foreign-pointer (root (foreign-type-size 'port))
+    (with-foreign-pointer (retry-name 2048)
+      (with-foreign-pointer (do-retry (foreign-type-size 'retry-type))
+        (let ((uids (get-foreign-uids user))
+              (gids (get-foreign-gids user)))
+          (with-cleanup (free-memory-list (list (first uids) (first gids)))
+            (let ((return-code (%fsys-getroot control
+                                              dotdot
+                                              dotdot-poly
+                                              (first uids) (second uids) ; pointer and size
+                                              (first gids) (second gids) ; pointer and size
+                                              flags
+                                              do-retry
+                                              retry-name
+                                              root)))
+              (select-error return-code (values
+                                          (mem-ref do-retry 'retry-type)
+                                          (foreign-string-to-lisp retry-name)
+                                          (mem-ref root 'port))))))))))
