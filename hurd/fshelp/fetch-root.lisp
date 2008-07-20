46d84a4d3bc35c9915bb18ae5a9116833c1789dc
Flavio Cruz <flaviocruz@gmail.com>
2008-07-20 21:14:46 +0100

    Completely rework fetch-root to work now.

--- hurd/fshelp/fetch-root.lisp
+++ hurd/fshelp/fetch-root.lisp
@@ -7,65 +7,60 @@
   err
   (dotdot port)
   (fetch-root-callback :pointer)
-  (node-port port)
-  (uid :unsigned-int)
-  (gid :unsigned-int)
-  (argz :string)
+  (uid uid-t)
+  (gid gid-t)
+  (argz :pointer)
   (argz-len :unsigned-int)
   (control-port port-pointer))
 
-(defun helper-fetch-root (dotdot fetch-root-callback node-port uid gid path)
-  (with-foreign-pointer (control (foreign-type-size 'port))
-		(let ((return-code (%helper-fetch-root dotdot fetch-root-callback
-                                           node-port uid gid path (+ 1 (length path)) control)))
-      (select-error return-code (mem-ref control 'port)))))
+(defun %fetch-root (dotdot fetch-root-callback uid gid translator)
+  (let* ((len-args (string-list-len translator))
+         (argz-len (sum-list len-args)))
+    (with-foreign-pointer (control (foreign-type-size 'port))
+      (with-foreign-pointer (argz argz-len)
+        (list-to-foreign-string-zero-separated translator argz len-args)
+        (let ((return-code (%helper-fetch-root dotdot
+                                               fetch-root-callback
+                                               uid
+                                               gid
+                                               argz
+                                               argz-len
+                                               control)))
+          (select-error return-code (mem-ref control 'port)))))))
 
-(defun try-start-translator (box dotdot fetch-root-callback
-                                 node-port path uid gid)
+(defun %try-start-translator (box dotdot fetch-root-callback path uid gid)
   (multiple-value-bind (ret err)
-    (helper-fetch-root dotdot
-                       fetch-root-callback
-                       node-port
-                       uid gid
-                       path)
-    (if err
-      (return-from try-start-translator err))
-    (if (port-valid ret)
-      ret
-      :translator-died)))
+    (%fetch-root dotdot fetch-root-callback uid gid path)
+    (cond
+      (err err)
+      ((port-valid-p ret) ret)
+      (t :translator-died))))
 
-(defun fetch-root (box port
-                       dotdot
-                       flags
-                       uids uids-count
-                       gids gids-count
-                       get-translator-callback
-                       fetch-root-callback
-                       retry
-                       retry-name
-                       retry-port)
+(defun fetch-root (box dotdot flags user get-translator-callback fetch-root-callback)
   (unless (box-translated-p box)
-    (multiple-value-bind (path uid gid) (funcall get-translator-callback (node box) port)
-      (unless path
+    (multiple-value-bind (path uid gid) (funcall get-translator-callback box)
+      (unless (and path uid gid)
         (return-from fetch-root path)) ; return error
-      (let ((control-port (try-start-translator box dotdot
-                                                fetch-root-callback
-                                                port
-                                                path
-                                                uid
-                                                gid)))
-        (unless (port-valid control-port)
-          (return-from fetch-root control-port)) ; error
-        (setf (active box) control-port))))
+      (let ((control (%try-start-translator box
+                                            dotdot
+                                            fetch-root-callback
+                                            path
+                                            uid
+                                            gid)))
+        (unless (port-valid-p control)
+          (return-from fetch-root control)) ; error
+        ; Set the now _active_ translator port
+        (setf (active box) control))))
+  ; If we have come this far, it means that the box has an active port now!
   (let ((control (active box)))
     (port-mod-refs control :right-send 1)
-    (let ((root (fsys-getroot control dotdot :copy-send
-                              uids uids-count
-                              gids gids-count
-                              flags retry retry-name)))
-      (when (null root)
-        (setf (active box) nil)
-        (port-deallocate control)
-        (return-from fetch-root nil))
-      (setf (mem-ref retry-port 'port) root)
-      t)))
+    (multiple-value-bind (retry retry-name port)
+        (fsys-getroot control dotdot :copy-send user flags)
+      (port-deallocate control)
+      (cond
+        ((not (port-valid-p port))
+         (box-set-active box nil nil)
+         nil)
+        (t
+          (values retry retry-name port))))))
+
