129fb796f845f0739fc6f9fe8508db53593d4984
Flavio Cruz <flaviocruz@gmail.com>
2008-07-20 21:18:08 +0100

    Put fetch-root callbacks in a single file.

---
+++ translator/interfaces/getroot-lookup-callbacks.lisp
@@ -0,0 +1,32 @@
+
+(in-package :hurd-translator)
+
+(defvar *current-node* nil)
+(defvar *current-dotdot* nil)
+
+(defun get-translator-callback (box)
+  (let ((node (node box)))
+    (cond
+      ((and (has-passive-trans-p (stat node))
+            (translator node))
+       (values (translator node)
+               (stat-get (stat node) 'uid)
+               (stat-get (stat node) 'gid)))
+      (t :no-such-file))))
+
+(defcallback fetch-root-callback
+             err
+             ((flags open-flags)
+              (underlying port-pointer)
+              (underlying-type :pointer))
+  (let* ((node *current-node*)
+         (stat (stat node))
+         (user (make-iouser :uids (stat-get stat 'uid)
+                            :gids (stat-get stat 'gid)))
+         (new-open-node (make-open-node node flags
+                                        :root-parent *current-dotdot*))
+         (new (new-protid *translator* user new-open-node)))
+    (setf (mem-ref underlying 'port) (get-right new))
+    (setf (mem-ref underlying-type 'msg-type-name) :make-send)
+    t))
+
