4e8ca04ea0c30b2135fd10a6f056aee94a03306e
Flavio Cruz <flaviocruz@gmail.com>
2008-07-21 20:10:05 +0000

    Add file-get-storage-info callback.

---
+++ translator/interfaces/file-get-storage-info.lisp
@@ -0,0 +1,32 @@
+
+(in-package :hurd-translator)
+
+(def-fs-interface :file-get-storage-info ((file port)
+                                          (ports :pointer)
+                                          (ports-type :pointer)
+                                          (num-ports :pointer)
+                                          (ints :pointer)
+                                          (num-ints :pointer)
+                                          (offsets :pointer)
+                                          (num-offsets :pointer)
+                                          (data :pointer)
+                                          (data-len :pointer))
+  (with-lookup protid file
+    (setf (mem-ref data-len 'msg-type-number) 0
+          (mem-ref num-offsets 'msg-type-number) 0
+          (mem-ref num-ports 'msg-type-number) 0)
+    (when (zerop (mem-ref num-ints 'msg-type-number))
+      (warn "mmap ints")
+      (setf (mem-ref ints :pointer)
+            (mmap (make-pointer 0)
+                  (foreign-type-size 'msg-type-number)
+                  '(:prot-read :prot-write)
+                  '(:map-anon)
+                  0
+                  0)))
+    (setf (mem-ref num-ints 'msg-type-number) 1)
+    (let ((firstptr (mem-ref ints :pointer)))
+      (warn "type is ~s" (storage *translator*))
+      (setf (mem-ref firstptr 'file-storage-class)
+            (storage *translator*)))
+    t))
