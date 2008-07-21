b04d46e8b6af747c90c51a22a87e3209288ec2d3
Flavio Cruz <flaviocruz@gmail.com>
2008-07-21 21:32:29 +0000

    Add file-get-translator RPC.

---
+++ hurd/fs/file-get-translator.lisp
@@ -0,0 +1,26 @@
+
+(in-package :hurd)
+
+(defcfun ("file_get_translator" %file-get-translator)
+  err
+  (file port)
+  (data :pointer)
+  (data-len :pointer))
+
+(defconstant +file-get-translator-initial-size+ 1024)
+
+(defun file-get-translator (file)
+  (let* ((orig (foreign-alloc :char :count +file-get-translator-initial-size+))
+         (buf (foreign-alloc :pointer))
+         (len (foreign-alloc 'msg-type-number
+                             :initial-element +file-get-translator-initial-size+)))
+    (setf (mem-ref buf :pointer) orig)
+    (with-cleanup (free-memory-list (list orig buf len))
+      (let ((err (%file-get-translator file buf len)))
+        (select-error
+          err
+          (let ((len (mem-ref len 'msg-type-number))
+                (ptr (mem-ref buf :pointer)))
+            (with-cleanup (unless (pointer-eq ptr orig) (munmap ptr len))
+              (foreign-string-zero-separated-to-list ptr len))))))))
+
