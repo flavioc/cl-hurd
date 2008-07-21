9409157ffde9dc88c4c241b8de83fd13f5c76190
Flavio Cruz <flaviocruz@gmail.com>
2008-07-21 20:40:38 +0000

    Remove comment.

--- translator/interfaces/io-stat.lisp
+++ translator/interfaces/io-stat.lisp
@@ -4,7 +4,6 @@
 (def-io-interface :io-stat ((io port)
 							(stat-info stat-t))
   (with-lookup protid io
-    ;(warn "stating node ~s" (get-node protid))
     (stat-copy stat-info (get-stat protid))
     (set-root stat-info nil)
     (set-active-trans stat-info
