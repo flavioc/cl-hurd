93baaf4506273128c31524e7a9e5696c5ee991c6
Flavio Cruz <flaviocruz@gmail.com>
2008-07-22 13:34:24 +0000

    Add file-chown RPC.

---
+++ hurd/fs/file-chown.lisp
@@ -0,0 +1,13 @@
+
+(in-package :hurd)
+
+(defcfun ("file_chown" %file-chown)
+  err
+  (file port)
+  (new-owner uid-t)
+  (new-group gid-t))
+
+(defun file-chown (file new-owner new-group)
+  (declare (type fixnum file new-owner new-group))
+  (let ((err (%file-chown file new-owner new-group)))
+    (select-error err t)))
