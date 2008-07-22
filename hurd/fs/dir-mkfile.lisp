cb7945dd426ba948f093601c80c19a191beafc0c
Flavio Cruz <flaviocruz@gmail.com>
2008-07-22 21:18:54 +0000

    Add dir-mkfile RPC.

---
+++ hurd/fs/dir-mkfile.lisp
@@ -0,0 +1,19 @@
+
+(in-package :hurd)
+
+(defcfun ("dir_mkfile" %dir-mkfile)
+  err
+  (dir port)
+  (flags open-flags)
+  (mode mode-t)
+  (newnode port-pointer))
+
+(defun dir-mkfile (dir &optional (flags nil) (mode (make-mode)))
+  (declare (type fixnum dir)
+           (type cons flags)
+           (type mode mode))
+  (with-foreign-pointer (newnode (foreign-type-size 'port))
+    (let ((err (%dir-mkfile dir flags mode newnode)))
+      (select-error err
+                    (mem-ref newnode 'port)))))
+
