42865bc50599803a5d20d1a980f928307db86b44
Flavio Cruz <flaviocruz@gmail.com>
2008-07-22 14:45:51 +0000

    Add file-lock RPC.

---
+++ hurd/fs/file-lock.lisp
@@ -0,0 +1,12 @@
+
+(in-package :hurd)
+
+(defcfun ("file_lock" %file-lock)
+  err
+  (file port)
+  (flags lock-flags))
+
+(defun file-lock (file flags)
+  (declare (type fixnum file)
+           (type cons flags))
+  (select-error (%file-lock file flags) t))
