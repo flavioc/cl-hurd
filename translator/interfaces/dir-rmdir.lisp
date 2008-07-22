360e56c9055ebda9739de4606617d3515837e1f0
Flavio Cruz <flaviocruz@gmail.com>
2008-07-22 21:22:49 +0000

    Easy dir-rmdir calls. Check if node is a directory, if the target exists, and if the target directory is empty.

--- translator/interfaces/dir-rmdir.lisp
+++ translator/interfaces/dir-rmdir.lisp
@@ -2,12 +2,25 @@
 (in-package :hurd-translator)
 
 (def-fs-interface :dir-rmdir ((port port)
-							  (name :string))
+                              (name :string))
   (with-lookup protid port
-    (if (remove-directory-entry *translator*
-                                (get-node protid)
-                                (get-user protid)
-                                name
-                                t)
-      t
-      :not-permitted)))
+    (block rmdir
+           (let ((node (get-node protid))
+                 (user (get-user protid)))
+             (unless (is-dir-p node)
+               (return-from rmdir :not-directory))
+             (let ((target (dir-lookup *translator* node user name)))
+               (unless target
+                 (return-from rmdir :no-such-file))
+               (unless (zerop (number-of-entries *translator*
+                                                 target
+                                                 user))
+                 (return-from rmdir :directory-not-empty))
+               (let ((err (remove-directory-entry *translator*
+                                                  node
+                                                  user
+                                                  name
+                                                  t)))
+                 (cond
+                   ((eq err t) t)
+                   (t err))))))))
