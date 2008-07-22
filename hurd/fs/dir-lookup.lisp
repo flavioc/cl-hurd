3246d2e9411efaa9b01a4b2f3e6044dd3a86bb04
Flavio Cruz <flaviocruz@gmail.com>
2008-07-22 19:14:33 +0000

    Add dir-lookup RPC.

---
+++ hurd/fs/dir-lookup.lisp
@@ -0,0 +1,29 @@
+
+(in-package :hurd)
+
+(defcfun ("dir_lookup" %dir-lookup)
+  err
+  (dir port)
+  (file-name :string)
+  (flags open-flags)
+  (mode mode-t)
+  (do-retry :pointer)
+  (retry-name :pointer)
+  (result port-pointer))
+
+(defun dir-lookup (dir file-name &key (flags '()) (mode (make-mode)))
+  (declare (type fixnum dir)
+           (type string file-name)
+           (type cons flags)
+           (type mode mode))
+  (with-foreign-pointer (do-retry (foreign-type-size 'retry-type))
+    (with-foreign-pointer (retry-name 1024) ; Same as libc's hurdlookup.c
+      (with-foreign-pointer (result (foreign-type-size 'port))
+        (let ((err (%dir-lookup dir file-name flags mode
+                                do-retry retry-name result)))
+          (select-error err
+                        (values
+                          (mem-ref do-retry 'retry-type)
+                          (mem-ref retry-name :string)
+                          (mem-ref result 'port))))))))
+
