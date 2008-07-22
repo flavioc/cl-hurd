b0454e71c54025390bb3ad1be0bbea473511c33d
Flavio Cruz <flaviocruz@gmail.com>
2008-07-22 21:19:13 +0000

    Add dir-mkdir RPC.

---
+++ hurd/fs/dir-mkdir.lisp
@@ -0,0 +1,13 @@
+
+(in-package :hurd)
+
+(defcfun ("dir_mkdir" %dir-mkdir)
+  err
+  (dir port)
+  (name :string)
+  (mode mode-t))
+
+(defun dir-mkdir (dir name &optional (mode (make-mode)))
+  (declare (type fixnum dir)
+           (type string name))
+  (select-error (%dir-mkdir dir name mode)))
