e77a46774c27cbab7a8dac7c907ff81e4cc8d8ec
Flavio Cruz <flaviocruz@gmail.com>
2008-07-22 13:17:01 +0000

    Add new specialized transbox to update active-translator mode field automagically.

---
+++ translator/transbox.lisp
@@ -0,0 +1,17 @@
+
+(in-package :hurd-translator)
+
+(defclass node-transbox (transbox)
+  ((node :initform nil
+         :initarg :node
+         :accessor node
+         :documentation "Node this refers to."))
+  (:documentation "Transbox used in nodes."))
+
+(defmethod box-set-active ((box node-transbox) port excl-p)
+  (let ((ret (call-next-method)))
+    (when ret
+      ; Update active-translator node stat field to most updated information.
+      (set-active-trans (stat (node box))
+                        (port-valid-p port)))
+    ret))
