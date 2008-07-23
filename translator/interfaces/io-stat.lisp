e778c6b92d84ba5a281a5c4f47e71706aa9be426
Flavio Cruz <flaviocruz@gmail.com>
2008-07-23 15:26:16 +0000

    Remove the passive and root bits changing code.

--- translator/interfaces/io-stat.lisp
+++ translator/interfaces/io-stat.lisp
@@ -5,9 +5,6 @@
 							(stat-info stat-t))
   (with-lookup protid io
     (stat-copy stat-info (get-stat protid))
-    (set-root stat-info nil)
-    (set-active-trans stat-info
-                      (box-translated-p (get-box protid)))
     (set-root stat-info
               (or (eq (get-node protid) (get-shadow-root protid))
                   (eq (get-node protid) (root *translator*))))
