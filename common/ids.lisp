3123794e952f6656935aef4f88b46483dfad143d
Flavio Cruz <flaviocruz@gmail.com>
2008-07-23 20:27:10 +0000

    Move uid/gid related functions to ids.

--- common/ids.lisp
+++ common/ids.lisp
@@ -12,3 +12,27 @@
 
 (defconstant +uid-t-size+ (foreign-type-size 'uid-t))
 (defconstant +gid-t-size+ (foreign-type-size 'gid-t))
+
+(defcfun ("getuid" %getuid) :unsigned-int)
+
+(defun getuid ()
+  "Get the real user ID of the process."
+  (%getuid))
+
+(defcfun ("geteuid" %geteuid) :unsigned-int)
+
+(defun geteuid ()
+  "Get the effective user ID of the process."
+  (%geteuid))
+
+(defcfun ("getgid" %getgid) :unsigned-int)
+
+(defun getgid ()
+  "Get the real group ID of the process."
+  (%getgid))
+
+(defcfun ("getegid" %getegid) :unsigned-int)
+
+(defun getegid ()
+  "Get the effective group ID of the process."
+  (%getegid))
