2291dc2f8ec2d626238e28771f29d3f2d5a52d99
Flavio Cruz <flaviocruz@gmail.com>
2008-07-23 14:44:56 +0000

    Add -p to predicate name.

--- translator/interfaces/file-chauthor.lisp
+++ translator/interfaces/file-chauthor.lisp
@@ -4,9 +4,9 @@
 (def-fs-interface :file-chauthor ((port port)
 								  (author uid-t))
   (with-lookup protid port
-    (when (allow-author-change *translator*
-                               (get-node protid)
-                               (get-user protid)
-                               author)
+    (when (allow-author-change-p *translator*
+                                 (get-node protid)
+                                 (get-user protid)
+                                 author)
       (setf (stat-get (stat (get-node protid)) 'author) author))
     t))
