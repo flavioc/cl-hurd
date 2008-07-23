7fc705644a7ce57dce91a301a113901e7f66f14a
Flavio Cruz <flaviocruz@gmail.com>
2008-07-23 00:12:50 +0000

    Use association lists, not hash tables.

--- translator/options.lisp
+++ translator/options.lisp
@@ -2,9 +2,9 @@
 (in-package :hurd-translator)
 
 (defclass translator-options ()
-  ((table :initform (make-hash-table :test 'equal)
-            :accessor table
-            :documentation "Hash table mapping option names and values."))
+  ((table :initform nil
+          :accessor table
+          :documentation "Alist mapping option names and values."))
   (:documentation "Class to manage translator options."))
 
 (defun %get-symbol-name (symbol)
@@ -18,44 +18,45 @@
 
 (defun %obj-to-string (obj)
   (cond
-    ((null obj)
-     "")
-    ((eq t obj)
-     "yes")
-    (t
-      (prin1-to-string obj))))
+    ((null obj) "")
+    ((eq t obj) "yes")
+    (t (prin1-to-string obj))))
 
 (defmethod get-translator-options ((options translator-options))
-  (loop for key being the hash-keys of (table options)
-        using (hash-value value)
+  (loop for (key . value) in (table options)
         collect (let ((base (concatenate 'string "--" key)))
                   (if (null value)
                     base
                     (concatenate 'string base "="
                                  (%obj-to-string value))))))
 
-(defmethod set-translator-options ((options translator-options) option-list &optional (clear-old t))
-  (with-accessors ((table table)) options
-    ; Clear old options.
-    (when clear-old
-      (clrhash table))
-    (loop for option in option-list
-          do (cond
-               ((listp option)
-                (setf (gethash (first option) table)
-                      (second option)))
-               (t
-                 (setf (gethash option table) nil))))
-    t))
+(defmethod set-translator-options ((options translator-options)
+                                   option-list &optional (clear-old t))
+  ; Clear old options.
+  (when clear-old
+    (setf (table options) nil))
+  (loop for option in option-list
+        do (cond
+             ((listp option)
+              (push (cons (first option) (second option))
+                    (table options)))
+             (t
+               (push (cons option nil) (table options)))))
+  t)
 
 (defmethod has-translator-option-p ((options translator-options) (option string))
-  (multiple-value-bind (val found-p)
-    (gethash option (table options))
-    found-p))
+  (if (assoc option (table options) :test #'equal)
+    t
+    nil))
 
 (defmethod get-translator-option ((options translator-options) (option string))
-  (gethash option (table options)))
+  (let ((found (assoc option (table options) :test #'equal)))
+    (when found
+      (if (cdr found)
+        (cdr found)
+        t))))
 
 (defmethod print-object ((options translator-options) stream)
   (format stream "#<translator-options ~s>"
           (get-translator-options options)))
+
