696a44aabf1d00ffee74f6fcb2fcfc456100e202
Flavio Cruz <flaviocruz@gmail.com>
2008-07-23 20:23:47 +0000

    Add io-identity.

---
+++ hurd/io/identity.lisp
@@ -0,0 +1,21 @@
+
+(in-package :hurd)
+
+(defcfun ("io_identity" %io-identity)
+  err
+  (file port)
+  (idport port-pointer)
+  (fsidport port-pointer)
+  (fileno :pointer))
+
+(defun io-identity (file)
+  (declare (type fixnum file))
+  (with-foreign-pointer (idport (foreign-type-size 'port))
+    (with-foreign-pointer (fsidport (foreign-type-size 'port))
+      (with-foreign-pointer (fileno (foreign-type-size 'ino-t))
+        (select-error (%io-identity file idport fsidport fileno)
+                      (values
+                        (mem-ref idport 'port)
+                        (mem-ref fsidport 'port)
+                        (mem-ref fileno 'ino-t)))))))
+
