15e1f90a7469016b6a65243bf78184c1f7e13060
Flavio Cruz <flaviocruz@gmail.com>
2008-07-23 14:39:50 +0000

    Typo: s/port/notify.

--- hurd/ports/dead-name.lisp
+++ hurd/ports/dead-name.lisp
@@ -5,10 +5,11 @@
 (def-notify-interface :do-mach-notify-dead-name
                       ((notify port) (name port))
   (let ((port-data (gethash notify *all-ports*)))
+    (warn "GOT DEAD NAME ~s ~s" notify name)
     (when (listp port-data)
       (let ((port-info (first port-data))
             (bucket (second port-data)))
-      (remhash port *all-ports*)
+      (remhash notify *all-ports*)
       (when bucket
         (remove-port bucket port-info nil))
       (port-deallocate name)))))
