38dbc2ee207bef1e0eb7f3c94f3754a0356dd77c
Flavio Cruz <flaviocruz@gmail.com>
2008-07-23 20:25:21 +0000

    Add io-read.

---
+++ hurd/io/read.lisp
@@ -0,0 +1,39 @@
+
+(in-package :hurd)
+
+(defcfun ("io_read" %io-read)
+  err
+  (file port)
+  (data :pointer)
+  (data-len :pointer)
+  (offset :unsigned-long-long)
+  (amount vm-size))
+
+(defconstant +default-io-read-size+ 1024)
+
+(defun io-read (file &key (offset +minus-one-ll+) ; -1.
+                     (amount +default-io-read-size+))
+  (declare (type fixnum file amount)
+           (type integer offset))
+  (when (<= amount 0)
+    (return-from io-read nil))
+  (let ((initial-read-size (if (> amount 0)
+                             (min amount +default-io-read-size+))))
+    (with-foreign-pointer (data initial-read-size)
+      (with-foreign-pointer (len (foreign-type-size 'msg-type-number))
+        (setf (mem-ref len 'msg-type-number) initial-read-size)
+        (with-foreign-pointer (ptr (foreign-type-size :pointer))
+          (setf (mem-ref ptr :pointer) data)
+          (let ((err (%io-read file ptr len offset amount)))
+            (select-error err
+                          (let ((new-ptr (mem-ref ptr :pointer))
+                                (total (mem-ref len 'msg-type-number)))
+                            (with-cleanup (unless (pointer-eq new-ptr data)
+                                            (munmap new-ptr total))
+                              (let ((ret (make-array total :fill-pointer 0
+                                                     :element-type '(unsigned-byte 8))))
+                                (loop for i from 0 below total
+                                      do (vector-push
+                                           (mem-aref new-ptr :unsigned-char i)
+                                           ret))
+                                ret))))))))))
