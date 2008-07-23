7a54e9f53db281b929c468611c7a9bfb01a844e5
Flavio Cruz <flaviocruz@gmail.com>
2008-07-23 15:25:43 +0000

    Implement the box-set-passivce, passive and box-passive-p to make changes the the translator passive bit when needed.

--- translator/transbox.lisp
+++ translator/transbox.lisp
@@ -9,6 +9,7 @@
   (:documentation "Transbox used in nodes."))
 
 (defmethod box-set-active ((box node-transbox) port excl-p)
+  "Update the active stat bit when we change the transbos port."
   (declare (ignore excl-p))
   (let ((ret (call-next-method)))
     (when ret
@@ -16,3 +17,23 @@
       (set-active-trans (stat (node box))
                         (port-valid-p port)))
     ret))
+
+(defmethod box-set-passive ((box node-transbox) path)
+  "Update the passive stat bit when we change the transbox path."
+  (let ((ret (call-next-method)))
+    (when ret
+      (set-passive-trans (stat (node box))
+                         (if (null path) nil t)))
+    ret))
+
+(defmethod passive ((box node-transbox))
+  "Only return the passive path when the stat bit is sane."
+  (cond
+    ((has-passive-trans-p (stat (node box)))
+     (call-next-method))
+    (t nil)))
+
+(defmethod box-passive-p ((box node-transbox))
+  "To be passive the stat bit must be set."
+  (and (call-next-method)
+       (has-passive-trans-p (stat (node box)))))
