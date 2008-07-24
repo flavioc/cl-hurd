97888e89ebb3e7e4a07ccb604b17f4ceeec1cabd
Flavio Cruz <flaviocruz@gmail.com>
2008-07-24 15:35:49 +0000

    Return T when return code is server-died or send-invalid-dest.

--- hurd/fsys/goaway.lisp
+++ hurd/fsys/goaway.lisp
@@ -1,7 +1,16 @@
 
 (in-package :hurd)
 
-(defcfun ("fsys_goaway" fsys-goaway)
+(defcfun ("fsys_goaway" %fsys-goaway)
   err
   (control port)
   (flags fsys-goaway-flags))
+
+(defun fsys-goaway (control flags)
+  (declare (type fixnum control)
+           (type list flags))
+  (let ((err (%fsys-goaway control flags)))
+    (when (or (eq err :server-died)
+              (eq err :send-invalid-dest))
+      (setf err t))
+    (select-error err)))
