1fd9db853639faa9cafcdc73c15fe0e26acd3c0e
Flavio Cruz <flaviocruz@gmail.com>
2008-07-24 15:35:14 +0000

    Add fsys-syncfs RPC.

---
+++ hurd/fsys/syncfs.lisp
@@ -0,0 +1,14 @@
+
+(in-package :hurd)
+
+(defcfun ("fsys_syncfs" %fsys-syncfs)
+  err
+  (fsys port)
+  (wait :boolean)
+  (do-children :boolean))
+
+(defun fsys-syncfs (fsys &key (wait t) (do-children t))
+  (declare (type fixnum fsys)
+           (type boolean wait do-children))
+  (select-error (%fsys-syncfs fsys wait do-children)))
+
