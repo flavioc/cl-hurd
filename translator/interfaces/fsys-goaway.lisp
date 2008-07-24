284966d7d5726b0643ac37549b71181f049e9c1b
Flavio Cruz <flaviocruz@gmail.com>
2008-07-24 15:43:17 +0000

    When fsys-goaway is successfull, change the transbox active port.

--- translator/interfaces/fsys-goaway.lisp
+++ translator/interfaces/fsys-goaway.lisp
@@ -7,18 +7,27 @@
           (is-dir-p (stat (root *translator*))))
     (return-from %shutdown :resource-busy))
   (when (flag-is-p flags :recurse)
-    (bucket-iterate (port-bucket *translator*)
-                    (lambda (port)
-                      (when (typep port 'protid)
-                        (let ((node (get-node port)))
-                          (when (box-active-p (box node))
-                            (with-port-deallocate (control (box-fetch-control (box node)))
-                              ; We will catch errors later on
-                              (fsys-goaway control flags)))))))
+    (bucket-iterate
+      (port-bucket *translator*)
+      (lambda (port)
+        (when (typep port 'protid)
+          (let ((node (get-node port)))
+            (when (box-active-p (box node))
+              (with-port-deallocate (control (box-fetch-control (box node)))
+                ; We will catch errors later on
+                (warn "killing a translator..")
+                (when (fsys-goaway control flags)
+                  (box-set-active (box node) nil nil))))))))
+    ; Wait for no-senders port notifications
     (wait :miliseconds 500))
+  (bucket-iterate (port-bucket *translator*)
+                  (lambda (port)
+                    (when (typep port 'protid)
+                      (warn "node ~s still active" (get-node port)))))
   (when (and
           (not (flag-is-p flags :force))
-          (plusp (bucket-count-type (port-bucket *translator*) 'protid)))
+          (plusp (bucket-count-type
+                   (port-bucket *translator*) 'protid)))
     (return-from %shutdown :resource-busy))
   (unless (flag-is-p flags :nosync)
     (file-syncfs *translator* (make-iouser-root) t t))
@@ -32,7 +41,6 @@
                                   (reply-type msg-type-name)
                                   (flags fsys-goaway-flags))
   (when (port-exists-p control)
-    (warn "flags ~s" flags)
     (let ((shut-ret (%shutdown flags)))
       (cond
         ((eq t shut-ret)
