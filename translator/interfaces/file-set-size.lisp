b0665fe2ef0624cc44456ddffb2ab0dbbd3a0d93
Flavio Cruz <flaviocruz@gmail.com>
2008-07-24 22:48:47 +0000

    Check for various return codes. Return not-permitted when it is NIL.

--- translator/interfaces/file-set-size.lisp
+++ translator/interfaces/file-set-size.lisp
@@ -5,8 +5,10 @@
 (def-fs-interface :file-set-size ((file port)
                                   (size loff-t))
   (with-lookup protid file
-    (when (file-change-size *translator*
-                            (get-node protid)
-                            (get-user protid)
-                            size)
-      t)))
+    (let ((err (file-change-size *translator*
+                                 (get-node protid)
+                                 (get-user protid)
+                                 size)))
+      (cond
+        ((eq err nil) :not-permitted)
+        (t t)))))
