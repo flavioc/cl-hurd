3b96a1efb8bcf9584e8005bfd6a6105e2d121aec
Flavio Cruz <flaviocruz@gmail.com>
2008-07-24 15:42:38 +0000

    Respect the do-children arguments, setting the translator options on children translators.

--- translator/interfaces/fsys-set-options.lisp
+++ translator/interfaces/fsys-set-options.lisp
@@ -1,42 +1,29 @@
 
 (in-package :hurd-translator)
 
-(defun %split-options (item)
-  "Split options, examples:
-'readonly' -> 'readonly'
-'max-files=5' -> ('max-files' '5')"
-  (let ((pos (position #\= item)))
-    (cond
-      ((null pos)
-       item)
-      (t
-        (list (subseq item 0 pos)
-              (let* ((value (subseq item (1+ pos)))
-                     (converted (read-from-string value)))
-                (if (symbolp converted)
-                  value
-                  converted)))))))
-
-;; FIXME: do children when do-children eq T.
 (def-fsys-interface :fsys-set-options ((fsys port)
                                        (reply port)
                                        (reply-type msg-type-name)
                                        (data :pointer)
                                        (data-len msg-type-number)
                                        (do-children :boolean))
-  (declare (ignore reply reply-type))
+  (declare (ignore reply reply-type do-children))
   (when (port-exists-p fsys)
-    (let* ((options-list (foreign-string-zero-separated-to-list
-                           data data-len))
-           (filtered-list ; Remove options without "--"
-             (remove-if-not (lambda (item)
-                              (and (> (length item) 2)
-                                   (string= "--"
-                                            (subseq item 0 2))))
-                            options-list))
-           (final-list ; Remove initial --
-             (mapcar (lambda (item)
-                       (%split-options (subseq item 2)))
-                     filtered-list)))
-      (set-options *translator* final-list)
-      t)))
+    (let ((new-options (get-foreign-options data data-len)))
+      (when do-children
+        (let (nodes-done)
+          (bucket-iterate
+            (port-bucket *translator*)
+            (lambda (port)
+              (when (typep port 'protid)
+                (let ((node (get-node port)))
+                  (when (and (box-active-p (box node))
+                             (not (member node nodes-done)))
+                    (warn "setting options on ~s" node)
+                    (fsys-set-options (box-fetch-control (box node))
+                                      :options new-options
+                                      :do-children t)
+                    (push node nodes-done))))))))
+      (set-options *translator* new-options))
+    t))
+
