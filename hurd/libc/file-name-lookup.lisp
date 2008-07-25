c0f2ecc278bf28abff457cd83cc8a02ce64f2f91
Flavio Cruz <flaviocruz@gmail.com>
2008-07-25 14:30:32 +0000

    Move file-name-lookup to libc and add a new key parameter under that calls file_name_lookup_under.

---
+++ hurd/libc/file-name-lookup.lisp
@@ -0,0 +1,23 @@
+
+(in-package :hurd)
+
+(defcfun ("file_name_lookup" %file-name-lookup)
+  file-t
+  (name :string)
+  (flags open-flags)
+  (mode mode-t))
+
+(defcfun ("file_name_lookup_under" %file-name-lookup-under)
+  file-t
+  (startdir file-t)
+  (name :string)
+  (flags open-flags)
+  (mode mode-t))
+
+(defun file-name-lookup (name &key
+                              (flags '(:read)) (mode nil)
+                              (under nil))
+  "Open a port to file 'name'."
+  (cond
+    (under (%file-name-lookup-under under name flags mode))
+    (t (%file-name-lookup name flags mode))))
