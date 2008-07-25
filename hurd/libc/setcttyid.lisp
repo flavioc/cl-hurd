42d246ab5090543c20c65d3cd15c8d4f5e0d8a42
Flavio Cruz <flaviocruz@gmail.com>
2008-07-25 14:31:51 +0000

    Add setcttyid.

---
+++ hurd/libc/setcttyid.lisp
@@ -0,0 +1,12 @@
+
+(in-package :hurd)
+
+(defcfun ("setcttyid" %setcttyid)
+  :int
+  (port port))
+
+(defun setcttyid (port)
+  (declare (type fixnum port))
+  (let ((err (%setcttyid port)))
+    (not (= err -1))))
+
