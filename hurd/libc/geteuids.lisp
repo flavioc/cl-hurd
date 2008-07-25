b6f696f36ceb8e7a6f0a0258552cf3df05729bd5
Flavio Cruz <flaviocruz@gmail.com>
2008-07-25 14:31:23 +0000

    Add geteuids.

---
+++ hurd/libc/geteuids.lisp
@@ -0,0 +1,16 @@
+
+(in-package :hurd)
+
+(defcfun ("geteuids" %geteuids)
+  :int
+  (n :int)
+  (uids :pointer))
+
+(defconstant +default-geteuids-size+ 20)
+
+(defun geteuids ()
+  (with-foreign-pointer (ptr (* +uid-t-size+
+                                +default-geteuids-size+))
+    (let ((total (%geteuids +default-geteuids-size+ ptr)))
+      (loop for i from 0 below total
+            collect (mem-aref ptr 'uid-t i)))))
