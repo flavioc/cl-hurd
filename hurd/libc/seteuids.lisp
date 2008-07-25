fca12c0aac0f086be8a069a7e4d89d9b6bf951b5
Flavio Cruz <flaviocruz@gmail.com>
2008-07-25 14:32:06 +0000

    Add seteuids.

---
+++ hurd/libc/seteuids.lisp
@@ -0,0 +1,23 @@
+
+(in-package :hurd)
+
+(defcfun ("seteuids" %seteuids)
+  :int
+  (n :int)
+  (ptr :pointer))
+
+(defun seteuids (uid-list)
+  (declare (type cons uid-list))
+  (unless (and (listp uid-list)
+               (not (null uid-list)))
+    (return-from seteuids nil))
+  (let ((total (length uid-list)))
+    (with-foreign-pointer (ptr (* +uid-t-size+
+                                  total))
+      (loop for i from 0 below total
+            for uid in uid-list
+            do (setf (mem-aref ptr 'uid-t i) uid))
+      (let ((err (%seteuids total ptr)))
+        (cond
+          ((= err -1) nil)
+          (t err))))))
