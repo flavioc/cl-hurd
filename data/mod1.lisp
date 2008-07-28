5edd8242bfefcdbfa7a791d950e06eafbe9d19f9
Flavio Cruz <flaviocruz@gmail.com>
2008-07-28 21:18:23 +0000

    New data file for the mod translator.

---
+++ data/mod1.lisp
@@ -0,0 +1,10 @@
+
+(:dir "root"
+      (:file "z" "ZZZzz")
+      (:dir "a")
+      (:dir "thing"
+            (:file "abc" ""))
+      (:file "f" "this file has no data")
+      (:file "b" "this is the b file!")
+      (:dir "i-am-bored"))
+
