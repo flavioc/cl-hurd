9152a8e936df070b0362819558dea7f0ebe4dedc
Flavio Cruz <flaviocruz@gmail.com>
2008-07-28 19:53:01 +0100

    No need to load libmachuser.

--- mach/mach.lisp
+++
@@ -1,8 +0,0 @@
-
-(in-package :mach)
-
-(define-foreign-library libmachuser
-  (:unix (:or "libmachuser-2.7.so" "libmachuser.so.1"))
-  (t (:default "libmachuser")))
-
-(use-foreign-library libmachuser)
