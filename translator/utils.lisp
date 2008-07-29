9bcc595ab6c7e9e6bfdc88e0efe7b1503bacd64d
Flavio Cruz <flaviocruz@gmail.com>
2008-07-29 00:12:55 +0000

    Move propagate-read-to-execute to tree-translator.

--- translator/utils.lisp
+++ translator/utils.lisp
@@ -1,16 +1,6 @@
 
 (in-package :hurd-translator)
 
-(defmethod propagate-read-to-execute ((stat stat))
-  "Enables the execute permission bit if the read bit is on."
-  (if (has-perms-p stat 'read 'owner)
-    (set-perms stat 'exec 'owner))
-  (if (has-perms-p stat 'read 'group)
-    (set-perms stat 'exec 'group))
-  (if (has-perms-p stat 'read 'others)
-    (set-perms stat 'exec 'others))
-  t)
-
 (defun port-exists-p (port)
   "Checks port existence on the actual translator."
   (has-port (port-bucket *translator*) port))
