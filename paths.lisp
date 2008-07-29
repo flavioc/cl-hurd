9bf4c2bbbeb1c1627870fe4b9929ed3afed260a7
Flavio Cruz <flaviocruz@gmail.com>
2008-07-29 18:23:28 +0000

    Update library patyh.

--- paths.lisp
+++ paths.lisp
@@ -3,10 +3,9 @@
 
 (let ((hurd-pathname (asdf:component-pathname
                        (asdf:find-system 'hurd))))
-  (push (merge-pathnames #p"hurd/helper-libs/"
+  (push (merge-pathnames #p"libs/"
                          hurd-pathname)
         cffi:*foreign-library-directories*)
   (push (merge-pathnames #p"stubs/"
                          hurd-pathname)
         cffi:*foreign-library-directories*))
-
