ee384325dd256d3b68131b90491b108f08a2d502
Flavio Cruz <flaviocruz@gmail.com>
2008-07-29 16:25:44 +0000

    s/add-control-port/bucket-add-control-port.

--- translator/interfaces/file-getcontrol.lisp
+++ translator/interfaces/file-getcontrol.lisp
@@ -10,7 +10,7 @@
            (unless (is-controller-p (get-node protid)
                                     (get-user protid))
              (return-from file-getcontrol :not-permitted))
-           (let ((port (add-control-port (port-bucket *translator*))))
+           (let ((port (bucket-add-control-port (port-bucket *translator*))))
              (setf (mem-ref control 'port) (get-right port))
              (setf (mem-ref control-type 'msg-type-name) :make-send)
              t))))
