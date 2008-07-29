a5b9ada0a79ac1991d2f45befde9ea7263e7ac39
Flavio Cruz <flaviocruz@gmail.com>
2008-07-29 13:24:37 +0000

    Add configure method. Pass fsys-startup flags to run-translator. Pass underlying-node to make-root-node.

--- translator/run.lisp
+++ translator/run.lisp
@@ -1,6 +1,14 @@
 
 (in-package :hurd-translator)
 
+(defmethod configure ((translator translator) flags)
+  "Gets the bootstrap port to call fsys-startup and installs a new control port into the bucket."
+  (with-port-deallocate (bootstrap (task-get-bootstrap-port))
+    (let ((port (add-control-port (port-bucket translator))))
+      (with-port-deallocate (right (get-send-right port))
+        (setf (slot-value translator 'underlying-node)
+              (fsys-startup bootstrap flags right :copy-send))))))
+
 (defmethod inner-run ((translator translator))
   "Run the translator server."
   (let ((*translator* translator))
@@ -9,15 +17,18 @@
                   (translator-demuxer in out))
                 (port-bucket *translator*))))
 
-(defmethod run-translator ((translator translator))
+(defmethod run-translator ((translator translator) &key flags)
   "Setup the translator and the run it."
+  (configure translator flags)
   (when (running-p translator)
     (let* ((under (underlying-node translator))
            (stat (io-stat under)))
       (set-trans stat nil)
       (set-root stat t)
       (setf (root translator)
-            (make-root-node translator (make-stat stat))))
+            (make-root-node translator
+                            (underlying-node translator)
+                            (make-stat stat))))
     (inner-run translator)))
 
 (defun calculate-miliseconds (seconds miliseconds)
