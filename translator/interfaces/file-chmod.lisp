c9086e8267a3054d8d490fcf24eb31aaeb016dad
Flavio Cruz <flaviocruz@gmail.com>
2008-07-29 16:26:44 +0000

    s/file-chmod/chmod-file.

--- translator/interfaces/file-chmod.lisp
+++ translator/interfaces/file-chmod.lisp
@@ -2,14 +2,14 @@
 (in-package :hurd-translator)
 
 (def-fs-interface :file-chmod ((file port)
-							   (mode mode-t))
+                               (mode mode-t))
   (with-lookup protid file
     ; We only want permission bits
     ; Disable those bits that don't make much sense here
     (set-spare mode nil)
     (set-types mode nil)
     (set-trans mode nil)
-    (let ((err (file-chmod *translator*
+    (let ((err (chmod-file *translator*
                            (get-node protid)
                            (get-user protid)
                            mode)))
