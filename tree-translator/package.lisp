db87f8187298141beee47c172eb57239caa41ff9
Flavio Cruz <flaviocruz@gmail.com>
2008-07-29 16:57:06 +0000

    Move tree-translator package to this file.

---
+++ tree-translator/package.lisp
@@ -0,0 +1,22 @@
+
+(in-package #:cl-user)
+
+(defpackage :cl-hurd.translator.tree
+  (:nicknames :hurd-tree-translator)
+  (:use :cl :hurd-common :mach :hurd :hurd-translator)
+  (:export :fill-root-node
+           :tree-translator
+           :dir-entry
+           :dir-size
+           :entry
+           :add-entry
+           :get-entry
+           :rename-dir-entry
+           :remove-dir-entry
+           :setup-entry
+           :iterate-entries
+           :iterate-entries-deep
+           :has-entry-p
+           :get-dir-entries
+           :parent))
+
