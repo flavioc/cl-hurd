78a3ac8886799666927049d72080d4ad8497b97d
Flavio Cruz <flaviocruz@gmail.com>
2008-07-29 18:27:42 +0000

    Move helper-libs to /libs/.

--- hurd/helper-libs/error.c
+++
@@ -1,9 +0,0 @@
-
-#include <errno.h>
-
-int
-get_hurd_error_code(const int id)
-{
-	/* wrap macro in the function */
-	return _HURD_ERRNO(id);
-}
--- hurd/helper-libs/round-page.c
+++
@@ -1,8 +0,0 @@
-
-#include <mach/vm_param.h>
-
-vm_offset_t
-helper_round_page(vm_offset_t address)
-{
-	return round_page(address);
-}
