1df530a0da3e9123f95ac5328d54f3b06d6d2800
Flavio Cruz <flaviocruz@gmail.com>
2008-07-29 18:19:55 +0000

    Use stat as parameter, not the node. Always subtract 2 from the number of directories.

--- translator/interfaces/dir-rmdir.lisp
+++ translator/interfaces/dir-rmdir.lisp
@@ -7,14 +7,15 @@
     (block rmdir
            (let ((node (get-node protid))
                  (user (get-user protid)))
-             (unless (is-dir-p node)
+             (unless (is-dir-p (stat node))
                (return-from rmdir :not-directory))
              (let ((target (directory-lookup *translator* node user name)))
                (unless target
                  (return-from rmdir :no-such-file))
-               (unless (zerop (number-of-entries *translator*
-                                                 target
-                                                 user))
+               (unless (zerop (- (number-of-entries *translator*
+                                                    target
+                                                    user)
+                                 2)) ; Don't count . and ..
                  (return-from rmdir :directory-not-empty))
                (let ((err (remove-directory-entry *translator*
                                                   node
