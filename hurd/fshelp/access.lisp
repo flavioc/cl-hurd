a920345fa687f437d000a84fd89cbcc5277eaa5f
Flavio Cruz <flaviocruz@gmail.com>
2008-07-29 00:12:11 +0000

    Use keywords. When root and permission type is :exec, check for it.

--- hurd/fshelp/access.lisp
+++ hurd/fshelp/access.lisp
@@ -3,16 +3,19 @@
 
 (defmethod has-access-p ((stat stat) (user iouser) flag)
   "Verify if user can operate 'flag' on file identified by stat.
-flag can be: 'read, 'write or 'exec."
+flag can be: :read, :write or :exec."
   (cond
-    ((contains-uid user 0) t)
+    ((contains-uid user 0)
+     (if (eq flag :exec)
+       (has-perms-p stat :exec)
+       t))
     ((and (empty-uids-p user)
           (is-useunk-p stat))
-     (has-perms-p stat flag 'unknown))
+     (has-perms-p stat flag :unknown))
     ((is-owner-p stat user)
-     (has-perms-p stat flag 'owner))
+     (has-perms-p stat flag :owner))
     ((contains-gid user (stat-get stat 'gid))
-     (has-perms-p stat flag 'group))
+     (has-perms-p stat flag :group))
     (t
-      (has-perms-p stat flag 'others))))
+      (has-perms-p stat flag :others))))
 
