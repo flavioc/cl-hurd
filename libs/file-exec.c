ece754e0633ac27a1b1deda9236a58926fa70daa
Flavio Cruz <flaviocruz@gmail.com>
2008-07-30 13:21:23 +0000

    Fork the process to execute a file. Implemente exec_finished as an interface for the lisp part.

--- libs/file-exec.c
+++ libs/file-exec.c
@@ -1,80 +1,10 @@
 
+#include <stdlib.h>
 #include <stdio.h>
 #include <hurd.h>
 #include <hurd/exec.h>
-#include <cthreads.h>
 
-typedef struct {
-  int ended;
-  kern_return_t err;
-  file_t execserver;
-  mach_port_t file;
-  mach_msg_type_name_t filePoly;
-  mach_port_t oldtask;
-  int flags;
-  data_t argv;
-  mach_msg_type_number_t argvCnt;
-  data_t envp;
-  mach_msg_type_number_t envpCnt;
-  portarray_t dtable;
-  mach_msg_type_name_t dtablePoly;
-  mach_msg_type_number_t dtableCnt;
-  portarray_t portarray;
-  mach_msg_type_name_t portarrayPoly;
-  mach_msg_type_number_t portarrayCnt;
-  intarray_t intarray;
-  mach_msg_type_number_t intarrayCnt;
-  mach_port_array_t deallocnames;
-  mach_msg_type_number_t deallocnamesCnt;
-  mach_port_array_t destroynames;
-  mach_msg_type_number_t destroynamesCnt;
-} exec_data;
-
-any_t
-thread_exec(any_t arg)
-{
-  //exec_data *data = (exec_data*)arg;
-  exec_data *data;
-
-fprintf(stderr, "starting exec_exec\n");
-  return(NULL);
-  /*
-
-  data->err =
-    exec_exec(data->execserver, data->file, data->filePoly,
-        data->oldtask, data->flags, data->argv, data->argvCnt,
-        data->envp, data->envpCnt, data->dtable, data->dtablePoly,
-        data->dtableCnt, data->portarray, data->portarrayPoly,
-        data->portarrayCnt, data->intarray, data->intarrayCnt,
-        data->deallocnames, data->deallocnamesCnt,
-        data->destroynames, data->destroynamesCnt);
-
-  fprintf(stderr, "exec_exec done\n");
-
-  data->ended = 1;
-
-  if(!data->err) {
-
-    unsigned int i;
-
-    mach_port_deallocate(mach_task_self(), data->oldtask);
-
-    for(i = 0; i < data->dtableCnt; ++i) {
-      mach_port_deallocate(mach_task_self(), data->dtable[i]);
-    }
-
-    for(i = 0; i < data->portarrayCnt; ++i) {
-      mach_port_deallocate(mach_task_self(), data->portarray[i]);
-    }
-
-  }
-
-  fprintf(stderr, "ending thread_exec\n");
-  return(NULL);
-  */
-}
-
-exec_data*
+pid_t
 do_exec_exec(file_t execserver0,
     mach_port_t file0,
     mach_msg_type_name_t filePoly0,
@@ -97,38 +27,64 @@ do_exec_exec(file_t execserver0,
     mach_port_array_t destroynames0,
     mach_msg_type_number_t destroynamesCnt0)
 {
-  exec_data *data = (exec_data*)malloc(sizeof(exec_data));
+  pid_t pid = fork();
+  unsigned i;
 
-  fprintf(stderr, "Data created\n");
+  if(pid == 0) { // child
+    error_t err;
+    pid_t parent = getppid();
+    task_t parent_task = pid2task(parent);
+    mach_msg_type_name_t foo;
+    mach_port_t new_execserver;
+    mach_port_t file_copy;
 
-  data->ended = 0;
-  data->err = 0;
-  data->execserver = execserver0;
-  data->file = file0;
-  data->filePoly = filePoly0;
-  data->oldtask = oldtask0;
-  data->flags = flags0;
-  data->argv = argv0;
-  data->argvCnt = argvCnt0;
-  data->envp = envp0;
-  data->envpCnt = envpCnt0;
-  data->dtable = dtable0;
-  data->dtablePoly = dtablePoly0;
-  data->dtableCnt = dtableCnt0;
-  data->portarray = portarray0;
-  data->portarrayPoly = portarrayPoly0;
-  data->portarrayCnt = portarrayCnt0;
-  data->intarray = intarray0;
-  data->intarrayCnt = intarrayCnt0;
-  data->deallocnames = deallocnames0;
-  data->deallocnamesCnt = deallocnamesCnt0;
-  data->destroynames = destroynames0;
-  data->destroynamesCnt = destroynamesCnt0;
+    err = mach_port_extract_right(parent_task, execserver0, MACH_MSG_TYPE_COPY_SEND, &new_execserver, &foo);
 
-  fprintf(stderr, "Running cthread_fork\n");
-  cthread_fork(thread_exec, NULL);
-  return(NULL);
+    if(!err) {
+      err = mach_port_extract_right(parent_task, file0, MACH_MSG_TYPE_COPY_SEND, &file_copy, &foo);
+    }
 
-  return(data);
+    if(!err) {
+      err = exec_exec(new_execserver, file_copy, filePoly0,
+          oldtask0, flags0, argv0, argvCnt0,
+          envp0, envpCnt0, dtable0, dtablePoly0,
+          dtableCnt0, portarray0, portarrayPoly0,
+          portarrayCnt0, intarray0, intarrayCnt0,
+          deallocnames0, deallocnamesCnt0,
+          destroynames0, destroynamesCnt0);
+    }
+
+    mach_port_deallocate(mach_task_self(), parent_task);
+    mach_port_deallocate(mach_task_self(), new_execserver);
+    mach_port_deallocate(mach_task_self(), file_copy);
+    mach_port_deallocate(mach_task_self(), oldtask0);
+
+    for(i = 0; i < dtableCnt0; ++i) {
+      mach_port_deallocate(mach_task_self(), dtable0[i]);
+    }
+
+    for(i = 0; i < portarrayCnt0; ++i) {
+      mach_port_deallocate(mach_task_self(), portarray0[i]);
+    }
+
+    exit(err);
+  }
+  mach_port_deallocate(mach_task_self(), oldtask0);
+  for(i = 0; i < dtableCnt0; ++i) {
+    mach_port_deallocate(mach_task_self(), dtable0[i]);
+  }
+
+  for(i = 0; i < portarrayCnt0; ++i) {
+    mach_port_deallocate(mach_task_self(), portarray0[i]);
+  }
+
+  return(pid);
 }
 
+int
+exec_finished(pid_t pid, int* status)
+{
+  int ret = waitpid(pid, &status, WNOHANG);
+
+  return(ret > 0);
+}
