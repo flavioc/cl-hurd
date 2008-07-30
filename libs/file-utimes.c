80a62076370f0c030e4af67c11c586a546b34996
Flavio Cruz <flaviocruz@gmail.com>
2008-07-30 21:30:33 +0000

    Use the GNU Coding Style.

--- libs/file-utimes.c
+++ libs/file-utimes.c
@@ -4,9 +4,8 @@
 
 /* Wrap file_utimes to pass pointers as arguments. */
 kern_return_t
-helper_file_utimes(file_t utimes_file,
-    time_value_t *new_atime,
-    time_value_t *new_mtime)
+helper_file_utimes (file_t utimes_file,
+		    time_value_t * new_atime, time_value_t * new_mtime)
 {
-  return(file_utimes(utimes_file, *new_atime, *new_mtime));
+  return (file_utimes (utimes_file, *new_atime, *new_mtime));
 }
--- libs/round-page.c
+++ libs/round-page.c
@@ -2,7 +2,7 @@
 #include <mach/vm_param.h>
 
 vm_offset_t
-helper_round_page(vm_offset_t address)
+helper_round_page (vm_offset_t address)
 {
-	return round_page(address);
+  return round_page (address);
 }
