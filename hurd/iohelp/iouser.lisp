8e5d4af3c2067aa888402eebf55810f154a08ce4
Flavio Cruz <flaviocruz@gmail.com>
2008-07-30 21:32:48 +0000

    Add gid-setid, uid-setid, clear-user, add-uid, add-gid, count-gids, count-ids. Add suffix -p.

--- hurd/iohelp/iouser.lisp
+++ hurd/iohelp/iouser.lisp
@@ -13,7 +13,7 @@
          :documentation "List of uids")
    (gids :initform (list)
          :initarg :gids
-         :reader gids
+         :accessor gids
          :documentation "List of gids"))
   (:documentation "Groups list of uids and gids representing an I/O user"))
 
@@ -40,8 +40,6 @@
   (let ((obj (make-instance 'iouser
                             :uids (%idptr-to-list uids-ptr uids-len)
                             :gids (%idptr-to-list gids-ptr gids-len))))
-    ;(%free-ptr uids-ptr uids-len)
-    ;(%free-ptr gids-ptr gids-len)
     obj))
 
 (defun make-iouser-root ()
@@ -60,11 +58,11 @@
          (ptr (foreign-alloc 'gid-t :initial-contents gids)))
     (list ptr total)))
 
-(defmethod contains-uid ((iouser iouser) uid)
+(defmethod contains-uid-p ((iouser iouser) uid)
   "Does it contain 'uid' in UIDs?"
   (member uid (uids iouser)))
 
-(defmethod contains-gid ((iouser iouser) gid)
+(defmethod contains-gid-p ((iouser iouser) gid)
   "Does it contain 'gid' in GIDs?"
   (member gid (gids iouser)))
 
@@ -76,6 +74,28 @@
   "Is the GID's set empty?"
   (null (gids iouser)))
 
+(defmethod count-uids ((user iouser))
+  "Returns number of uids in user."
+  (length (uids user)))
+
+(defmethod count-gids ((user iouser))
+  "Returns number of gids in user."
+  (length (gids user)))
+
+(defmethod add-uid ((user iouser) uid)
+  "Add new uid to user."
+  (push uid (uids user)))
+
+(defmethod add-gid ((user iouser) gid)
+  "Add new gid to user."
+  (push gid (gids user)))
+
+(defmethod clear-user ((user iouser))
+  "Clear the UID+GID sets."
+  (setf (uids user) (list))
+  (setf (gids user) (list))
+  user)
+
 (defmethod restrict-iouser ((user1 iouser) (user2 iouser))
   "Returns an intersection of user1 and user2, except when user1 is root and then we return user2."
   (cond
@@ -86,8 +106,42 @@
                    :gids (intersection (gids user1)
                                        (gids user2))))))
 
+(defmethod uid-setid ((eff-user iouser) (avail-user iouser) id)
+  "Does the same thing as idvec_setid only for UID set.
+Set the first element of UID's eff-user set as id.
+When the avail-user is not empty, set the second element as id."
+  (let ((secure (and (not (contains-uid-p eff-user id))
+                     (not (contains-uid-p avail-user id)))))
+    (cond
+      ((empty-uids-p eff-user) (add-uid eff-user id))
+      (t
+        (add-uid avail-user id)
+        (setf (first (uids eff-user)) id)))
+    (unless (empty-uids-p avail-user)
+      (cond
+        ((> (count-uids avail-user) 1)
+         (setf (second (uids avail-user)) id))
+        (t (add-uid avail-user id))))
+    secure))
+
+(defmethod gid-setid ((eff-user iouser) (avail-user iouser) id)
+  "Does the same thing as idvec_setid, only for the GID set."
+  (let ((secure (and (not (contains-gid-p eff-user id))
+                     (not (contains-gid-p avail-user id)))))
+    (cond
+      ((empty-gids-p eff-user) (add-gid eff-user id))
+      (t
+        (add-gid avail-user id)
+        (setf (first (gids eff-user)) id)))
+    (unless (empty-gids-p avail-user)
+      (cond
+        ((> (count-gids avail-user) 1)
+         (setf (second (gids avail-user)) id))
+        (t (add-gid avail-user id))))
+    secure))
+
 (defmethod print-object ((iouser iouser) stream)
   "Print an iouser object."
   (format stream
-          "#<iouser: uids -> ~s ; gids -> ~s>"
+          "#<iouser: uids=~s gids=~s>"
           (uids iouser) (gids iouser)))
