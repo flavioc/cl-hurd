78c2b16dadfc5fc6a7a7ebf044356093f05f2af2
Flavio Cruz <flaviocruz@gmail.com>
2008-07-30 21:34:00 +0000

    Remove messages.

--- translator/interfaces/io-reauthenticate.lisp
+++ translator/interfaces/io-reauthenticate.lisp
@@ -7,11 +7,9 @@
     (let ((new-protid (new-protid *translator*
                                   (get-user protid)
                                   (open-node protid))))
- ;     (warn "going to reauth")
       (with-port-deallocate (new-right (get-send-right new-protid))
         (with-port-deallocate (rendezvous rend-port)
           (let ((new-user (user-reauth +auth-server+ rendezvous new-right t)))
-;            (warn "new-user ~s" new-user)
             (when new-user
               (setf (user new-protid) new-user))
             t))))))
