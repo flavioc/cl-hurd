141f68197bb329f7f78ae4d2aaa4b58e1cd700b1
Flavio Cruz <flaviocruz@gmail.com>
2008-08-04 20:51:15 +0000

    Use bitfields.

--- mach/port-type.lisp
+++ mach/port-type.lisp
@@ -42,64 +42,21 @@
 (defconstant +type-marequest+ #x40000000)
 (defconstant +type-compat+ #x20000000)
 
-;;
-;; Group everything in a list for easy manipulation
-;; in type translation.
-;;
-(defconstant +port-type-codes+
-  `((0 :type-none)
-    (,+type-send+ :type-send)
-    (,+type-receive+ :type-receive)
-    (,+type-send-once+ :type-send-once)
-    (,+type-port-set+ :type-port-set)
-    (,+type-dead-name+ :type-dead-name)
-    (,+type-send-receive+ :type-send-receive)
-    (,+type-send-rights+ :type-send-rights)
-    (,+type-port-rights+ :type-port-rights)
-    (,+type-port-or-dead+ :type-port-or-dead)
-    (,+type-all-rights+ :type-all-rights)
-    (,+type-dnrequest+ :type-dnrequest)
-    (,+type-compat+ :type-compat)))
+(defmacro %create-port-type-type ()
+  `(defbitfield port-type-t
+    (:type-none 0)
+    (:type-send ,+type-send+)
+    (:type-receive ,+type-receive+)
+    (:type-send-once ,+type-send-once+)
+    (:type-port-set ,+type-port-set+)
+    (:type-dead-name ,+type-dead-name+)
+    (:type-send-receive ,+type-send-receive+)
+    (:type-send-rights ,+type-send-rights+)
+    (:type-port-rights ,+type-port-rights+)
+    (:type-port-or-dead ,+type-port-or-dead+)
+    (:type-all-rights ,+type-all-rights+)
+    (:type-dnrequest ,+type-dnrequest+)
+    (:type-compat ,+type-compat+)))
 
-(define-foreign-type port-type-single-type ()
-  ()
-  (:documentation "CFFI type for simple port types.")
-  (:actual-type :unsigned-int)
-  (:simple-parser port-type-single-t))
+(%create-port-type-type)
 
-(defun translate-type-bits (value)
-  (translate-foreign-list value +port-type-codes+ :to))
-
-(defmethod translate-from-foreign (value (type port-type-single-type))
-  "Translate a foreign value into a symbol."
-  (translate-foreign-list value +port-type-codes+ :from))
-
-(defmethod translate-to-foreign (value (type port-type-single-type))
-  "Translate a symbol into a foreign value."
-  (translate-type-bits value))
-
-(defclass port-type-class ()
-  ((value :initarg :value
-          :accessor value))
-  (:documentation "Port type object for port types."))
-
-(defmethod port-type-is-p ((ptype port-type-class) flag)
-  "Checks if the port type object as a flag activated in its bitfield."
-  (let ((val (value ptype))
-        (bits (translate-type-bits flag)))
-    (when bits
-      (eq bits (boole boole-and val bits)))))
-
-(define-foreign-type port-type-type ()
-  ()
-  (:documentation "CFFI type for port types.")
-  (:actual-type :unsigned-int)
-  (:simple-parser port-type-t))
-
-(defmethod translate-from-foreign (value (type port-type-type))
-  "Translate a foreign bitfield into a port type object."
-  (make-instance 'port-type-class :value value))
-
-(defmethod translate-to-foreign (val (type port-type-type))
-  "Translate a port type object into a foreign bitfield."
-  (value val))
