e4d10c4f3839579c60140335dcd48c2d5e266669
Flavio Cruz <flaviocruz@gmail.com>
2008-08-06 01:40:03 +0000

    Add write support.

--- examples/test.lisp
+++ examples/test.lisp
@@ -9,6 +9,13 @@
 (assert (= (length ext:*args*) 1))
 (defconstant +file+ (first ext:*args*))
 
+(defun %create-data-array (size contents)
+  (make-array size
+              :initial-contents contents
+              :adjustable t
+              :fill-pointer t
+              :element-type '(unsigned-byte 8)))
+
 (defclass test-translator (tree-translator)
   ((file-stat :initarg :file-stat
               :initform nil
@@ -17,20 +24,10 @@
              :initform nil
              :accessor dir-stat)))
 
-(defclass mod-entry (entry)
+(defclass test-entry (entry)
   ((contents :initarg :data
-             :initform nil
+             :initform (%create-data-array 0 nil)
              :accessor data)))
-
-(define-callback allow-open-p test-translator
-                 (node user flags is-new-p)
-  (declare (ignore is-new-p))
-  (when (flag-is-p flags :write)
-    (return-from allow-open-p nil))
-  (when (flag-is-p flags :read)
-    (unless (has-access-p node user :read)
-      (return-from allow-open-p nil)))
-  t)
 
 (define-callback read-file test-translator
                  (node user start amount stream)
@@ -43,19 +40,64 @@
              (end (+ start total)))
         (write-sequence (subseq (data node) start end)
                         stream)
-        ; Also write newline.
-        (write-byte #x0A stream)
         t))))
 
-(define-callback shutdown test-translator ()
-  (warn "Mod translator going down!")
-  t)
+(defun %read-sequence (stream)
+  (let ((arr (%create-data-array 0 '())))
+    (loop for c = (read-byte stream nil)
+          while c
+          do (vector-push-extend c arr))
+    arr))
 
-(defun %create-data-array (size contents)
-  (make-array size
-              :initial-contents contents
-              :adjustable nil
-              :element-type '(unsigned-byte 8)))
+(define-callback write-file test-translator
+                 (node user offset stream)
+  (unless (has-access-p node user :write)
+    (return-from write-file nil))
+  (when (is-dir-p (stat node))
+    (return-from write-file :is-a-directory))
+  (let* ((size (stat-get (stat node) 'st-size))
+         (arr (%read-sequence stream))
+         (amount (length arr))
+         (final-size (max (+ amount offset) size)))
+    (unless (= final-size size)
+      (adjust-array (data node)
+                    final-size
+                    :fill-pointer t))
+    (loop for octet across arr
+          for i from offset
+          do (progn
+               (setf (aref (data node) i) octet)))
+    ; Update stat size.
+    (setf (stat-get (stat node) 'st-size) final-size)
+    t))
+
+(define-callback file-change-size test-translator
+                 (node user new-size)
+  (when (is-dir-p (stat node))
+    (return-from file-change-size :is-a-directory))
+  (when (is-owner-p node user)
+    (adjust-array (data node) new-size :fill-pointer t)
+    t))
+
+(define-callback create-file test-translator
+                 (node user filename mode)
+  (unless (has-access-p node user :write)
+    (return-from create-file nil))
+  (let ((entry (make-instance 'test-entry
+                              :stat (make-stat (stat node)
+                                               :mode mode
+                                               :size 0)
+                              :parent node)))
+    (add-entry node entry filename)
+    entry))
+
+(define-callback create-anonymous-file test-translator
+                 (node user mode)
+  (declare (ignore user))
+  (make-instance 'test-entry
+                 :stat (make-stat (stat node)
+                                  :mode mode)
+                 :parent node))
 
 (defun %read-file-data (str)
   (%create-data-array (length str)
@@ -76,13 +118,22 @@
                 do (%fill-node translator item dir))))
       (:file
         (let* ((data (first args))
-               (file (make-instance 'mod-entry
+               (file (make-instance 'test-entry
                                     :stat (make-stat
                                             (file-stat translator)
                                             :size (length data))
                                     :parent node
                                     :data (%read-file-data data))))
-          (add-entry node file name))))))
+          (add-entry node file name)))
+      (:link
+        (let ((target (first args))
+               (new (make-instance 'entry
+                                    :stat (make-stat
+                                            (file-stat translator)
+                                            :type :lnk)
+                                    :parent node)))
+          (setf (link new) target)
+          (add-entry node new name))))))
 
 (define-callback fill-root-node test-translator
                  ((node dir-entry))
@@ -98,7 +149,6 @@
                                                (:group :read :exec)))
                      :type :dir))
     (%fill-node translator data node)))
-
 
 (defun %read-lisp-file ()
   (with-open-file (stream +file+)
