0fce254224094a394dbeae18ac9b311cfcd7d006
Flavio Cruz <flaviocruz@gmail.com>
2008-08-06 01:31:30 +0000

    Use key arguments.

--- hurd/fs/dir-mkfile.lisp
+++ hurd/fs/dir-mkfile.lisp
@@ -8,11 +8,11 @@
   (mode mode-t)
   (newnode port-pointer))
 
-(defun dir-mkfile (dir &optional (flags nil) (mode (make-mode)))
+(defun dir-mkfile (dir &key (flags nil) (mode (make-mode)))
   (declare (type fixnum dir)
            (type list flags)
            (type mode mode))
   (with-foreign-pointer (newnode (foreign-type-size 'port))
     (select-error (%dir-mkfile dir flags mode newnode)
-				  (mem-ref newnode 'port))))
+                  (mem-ref newnode 'port))))
 
