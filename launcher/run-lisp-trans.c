ecaff659093c0389c73099881d870d72f029bc69
Flavio Cruz <flaviocruz@gmail.com>
2008-08-06 19:38:53 +0000

    Change run-lisp-trans to be a simple clisp wrapper. Check if the FD's are open first.

--- launcher/run-lisp-trans.c
+++ launcher/run-lisp-trans.c
@@ -1,177 +1,71 @@
 
 #include <unistd.h>
 #include <stdlib.h>
-#include <argp.h>
 #include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <paths.h>
-
-#include <mach/mach.h>
+#include <errno.h>
+#include <stdio.h>
 #include <error.h>
 
-/* general program information */
-const char *argp_program_version =
-"run-lisp-trans 1.0";
-const char *argp_program_bug_address =
-"<FIXME@gnu.org>";
-
-/* program documentation */
-static char doc[] =
-"run-lisp-trans -- startups an Hurd translator written in Common Lisp";
-
-/* a description of the arguments we accept. */
-static char args_doc[] = "LISP-FILE";
-
-static struct argp_option options[] = {
-	{"stdout", 'o', "FILE", 0, "File attached to standard output", 0},
-	{"stdin", 'i', "FILE", 0, "File attached to standard input", 0},
-	{ NULL, 0, NULL, 0, NULL, 0 }
-};
-
-/* struct where we save args configuration */
-struct arguments
-{
-	char *file;
-	char *new_stdout;
-	char *new_stdin;
-};
-
 /* lisp interpreter absolute location */
-static const char* LISP_PATH = "/usr/bin/clisp";
+static char *LISP_PATH = "/usr/bin/clisp";
 
-/* arg parser actions */
-static error_t
-parse_opt(int key, char *arg, struct argp_state *state)
-{
-	struct arguments *arguments = state->input;
-
-	switch(key) {
-		case 'o':
-			arguments->new_stdout = arg;
-			break;
-		case 'i':
-			arguments->new_stdin = arg;
-			break;
-		case ARGP_KEY_ARG:
-			if(arguments->file == NULL) {
-				arguments->file = arg;
-			}
-			break;
-		case ARGP_KEY_END:
-			if(state->arg_num == 0) {
-				argp_usage(state);
-			}
-			break;
-		default:
-			return ARGP_ERR_UNKNOWN;
-	}
-
-	return(0);
-}
-
-/* argp parser */
-static struct argp argp = { options, parse_opt, args_doc, doc, NULL, NULL, NULL };
-
-/* test if we are running as a translator */
 static int
-running_as_translator(void)
+fd_is_open (const int fd)
 {
-	mach_port_t bootstrap;
+  static struct stat buf;
 
-	task_get_bootstrap_port(mach_task_self(), &bootstrap);
-
-	const int is_translator = bootstrap != MACH_PORT_NULL;
-
-#ifndef NDEBUG
-	fprintf(stderr, "Bootstrap port is %d, so %s\n", bootstrap,
-			is_translator ? "we are running as a translator" :
-							"we are not running as a translator!");
-#endif
-	
-	return(is_translator);
-}
-
-static inline const char*
-get_file_path(const char *path)
-{
-	return(path == NULL ? _PATH_DEVNULL : path);
+  return (fstat (fd, &buf) == 0);
 }
 
 static void
-run_translator(struct arguments *arguments)
+run_lisp (int argc, char **argv)
 {
-	int stdin_handle, stdout_handle;
+  int stdin_handle, stdout_handle;
+  char *new_argv[argc + 1];
+  int i;
 
-	/* because fshelp closes stdin + stdout (only stderr available)
-	 * when starting translators and clisp
-	 * has deep dependencies on using these file descriptors
-	 * we give clisp the illusion that he is writing on the real ones,
-	 * opening files for each
-	 * (fd's 0 and 1 are free, so 'open' will assign them the right order)
-	 */
+  if (!fd_is_open (STDIN_FILENO))
+    {
+      stdin_handle = open (_PATH_DEVNULL, O_RDONLY);
 
-	/* now, we open the first file for stdin (fd = 0).
-	 * when the caller doesn't want to use a file, we open /dev/null
-	 */
-	stdin_handle = open(get_file_path(arguments->new_stdin), O_RDONLY);
-
-	if(stdin_handle == -1) {
-		error(EXIT_FAILURE, errno, "Could not open stdin file %s",
-				get_file_path(arguments->new_stdin));
+      if (stdin_handle == -1)
+	{
+	  error (EXIT_FAILURE, errno, "Could not open file %s",
+		 _PATH_DEVNULL);
 	}
+    }
 
-#ifndef NDEBUG
-	fprintf(stderr, "Stdin handle attached to fd %d: %s\n", stdin_handle,
-			get_file_path(arguments->new_stdin));
-#endif
+  if (!fd_is_open (STDOUT_FILENO))
+    {
+      stdout_handle = open (_PATH_DEVNULL, O_WRONLY);
 
-	/* time for stdout! */
-	stdout_handle = open(get_file_path(arguments->new_stdout), O_WRONLY | O_APPEND | O_CREAT, 0644);
-
-	if(stdout_handle == -1) {
-		error(EXIT_FAILURE, errno, "Could not open stdout file %s",
-				get_file_path(arguments->new_stdout));
+      if (stdout_handle == -1)
+	{
+	  error (EXIT_FAILURE, errno, "Could not open file %s",
+		 _PATH_DEVNULL);
 	}
+    }
 
-#ifndef NDEBUG
-	fprintf(stderr, "Stdout handle attached to fd %d: %s\n", stdout_handle,
-			get_file_path(arguments->new_stdout));
-#endif
+  new_argv[0] = LISP_PATH;
 
-	/* by this moment clisp is ready to run */
-	execl(LISP_PATH, LISP_PATH, arguments->file, NULL);
+  for (i = 1; i <= argc; ++i)
+    {
+      new_argv[i] = argv[i];
+    }
 
-	/* we should not get here ... */
-	error(EXIT_FAILURE, errno, "Could not launch lisp %s", LISP_PATH);
+  execvp (new_argv[0], new_argv);
+
+  /* we should not get here ... */
+  error (EXIT_FAILURE, errno, "Could not launch lisp %s", LISP_PATH);
 }
 
 int
-main(int argc, char **argv)
+main (int argc, char **argv)
 {
-	struct arguments arguments;
+  run_lisp (argc, argv);
 
-	arguments.file = NULL;
-	arguments.new_stdin = NULL;
-	arguments.new_stdout = NULL;
-
-	argp_parse(&argp, argc, argv, 0, 0, &arguments);
-
-	/* verify that all the needed arguments were
-	 * given by the user
-	 */
-	if(arguments.file == NULL) {
-		error(EXIT_FAILURE, 0, "A lisp file must be supplied");
-	}
-
-	/* first we check that we are really running as a translator.
-	 * we don't wanna be opening files and executing
-	 * clisp when it's not really needed.
-	 */
-	if(!running_as_translator()) {
-		argp_help(&argp, stderr, ARGP_HELP_USAGE, NULL);
-		error(EXIT_FAILURE, 0, "Must be started as a translator");
-	}
-
-	run_translator(&arguments);
-
-	return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
