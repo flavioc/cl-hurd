2ca792aa1e1c214199e5f6c2802ca61fc271690a
Flavio Cruz <flaviocruz@gmail.com>
2008-08-06 21:11:07 +0000

    Declare foreign codes inside the macro.

--- mach/msg-notify.lisp
+++ mach/msg-notify.lisp
@@ -5,17 +5,15 @@
 ;; This file defines the msg notify foreign codes.
 ;;
 
-;; Possible codes follow...
-(defconstant +notify-first+ #o100)
-(defconstant +notify-port-destroyed+ (+ +notify-first+ #o5))
-(defconstant +notify-dead-name+ (+ +notify-first+ #o10))
-(defconstant +notify-no-senders+ (+ +notify-first+ #o6))
-
 (defmacro %create-msg-notify ()
-  `(defcenum msg-notify
-    (:notify-port-destroyed ,+notify-port-destroyed+)
-    (:notify-dead-name ,+notify-dead-name+)
-    (:notify-no-senders ,+notify-no-senders+)))
+  (let* ((+notify-first+ #o100)
+         (+notify-port-destroyed+ (+ +notify-first+ #o5))
+         (+notify-dead-name+ (+ +notify-first+ #o10))
+         (+notify-no-senders+ (+ +notify-first+ #o6)))
+    `(defcenum msg-notify
+       (:notify-port-destroyed ,+notify-port-destroyed+)
+       (:notify-dead-name ,+notify-dead-name+)
+       (:notify-no-senders ,+notify-no-senders+))))
 
 (%create-msg-notify)
 
