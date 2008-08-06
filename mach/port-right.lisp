d9c6aae3d78e0c677604879aab537a0d7329a77f
Flavio Cruz <flaviocruz@gmail.com>
2008-08-06 21:11:48 +0000

    Declare foreign codes inside the macro.

--- mach/port-right.lisp
+++ mach/port-right.lisp
@@ -1,21 +1,11 @@
 
 (in-package :mach)
 
-(defconstant +right-send+ 0)
-(defconstant +right-receive+ 1)
-(defconstant +right-send-once+ 2)
-(defconstant +right-port-set+ 3)
-(defconstant +right-dead-name+ 4)
-(defconstant +right-number+ 5)
-
-(defmacro %create-port-right-type ()
-  `(defcenum port-right
-    (:right-send ,+right-send+)
-    (:right-receive ,+right-receive+)
-    (:right-send-once ,+right-send-once+)
-    (:right-port-set ,+right-port-set+)
-    (:right-dead-name ,+right-dead-name+)
-    (:right-number ,+right-number+)))
-
-(%create-port-right-type)
+(defcenum port-right
+  :right-send
+  :right-receive
+  :right-send-once
+  :right-port-set
+  :right-dead-name
+  :right-number)
 
--- mach/port-type.lisp
+++ mach/port-type.lisp
@@ -5,58 +5,47 @@
 ;; In this file we define the foreign type mach_port_type_t.
 ;;
 
-(defun %mach-port-type-get (code)
-  "Does the same as MACH_PORT_TYPE(right) from mach/port.h"
-  (ash 1 (+ code 16)))
-
-;;
-;; The bits for each type follow...
-;;
-
-(defconstant +type-send+ (%mach-port-type-get +right-send+))
-(defconstant +type-receive+ (%mach-port-type-get +right-receive+))
-(defconstant +type-send-once+ (%mach-port-type-get +right-send-once+))
-(defconstant +type-port-set+ (%mach-port-type-get +right-port-set+))
-(defconstant +type-dead-name+ (%mach-port-type-get +right-dead-name+))
-(defconstant +type-send-receive+
-  (boole boole-ior
-         +type-send+
-         +type-receive+))
-(defconstant +type-send-rights+
-  (boole boole-ior
-         +type-send+
-         +type-send-once+))
-(defconstant +type-port-rights+
-  (boole boole-ior
-         +type-send-rights+
-         +type-receive+))
-(defconstant +type-port-or-dead+
-  (boole boole-ior
-         +type-port-rights+
-         +type-dead-name+))
-(defconstant +type-all-rights+
-  (boole boole-ior
-         +type-port-or-dead+
-         +type-port-set+))
-(defconstant +type-dnrequest+ #x80000000)
-(defconstant +type-marequest+ #x40000000)
-(defconstant +type-compat+ #x20000000)
-
 (defmacro %create-port-type-type ()
-  `(defbitfield port-type-t
-    (:type-none 0)
-    (:type-send ,+type-send+)
-    (:type-receive ,+type-receive+)
-    (:type-send-once ,+type-send-once+)
-    (:type-port-set ,+type-port-set+)
-    (:type-dead-name ,+type-dead-name+)
-    (:type-send-receive ,+type-send-receive+)
-    (:type-send-rights ,+type-send-rights+)
-    (:type-port-rights ,+type-port-rights+)
-    (:type-port-or-dead ,+type-port-or-dead+)
-    (:type-all-rights ,+type-all-rights+)
-    (:type-dnrequest ,+type-dnrequest+)
-    (:type-compat ,+type-compat+)))
+  (flet ((%mach-port-type-get (code)
+                              (ash 1 (+ (foreign-enum-value 'port-right code) 16))))
+    (let* ((+type-none+ 0)
+           (+type-send+ (%mach-port-type-get :right-send))
+           (+type-receive+ (%mach-port-type-get :right-receive))
+           (+type-send-once+ (%mach-port-type-get :right-send-once))
+           (+type-port-set+ (%mach-port-type-get :right-port-set))
+           (+type-dead-name+ (%mach-port-type-get :right-dead-name))
+           (+type-send-receive+ (boole boole-ior
+                                       +type-send+
+                                       +type-receive+))
+           (+type-send-rights+ (boole boole-ior
+                                      +type-send+
+                                      +type-send-once+))
+           (+type-port-rights+ (boole boole-ior
+                                      +type-send-rights+
+                                      +type-receive+))
+           (+type-port-or-dead+ (boole boole-ior
+                                       +type-port-rights+
+                                       +type-dead-name+))
+           (+type-all-rights+ (boole boole-ior
+                                     +type-port-or-dead+
+                                     +type-port-set+))
+           (+type-dnrequest+ #x80000000)
+           (+type-marequest+ #x40000000)
+           (+type-compat+ #x20000000))
+      `(defbitfield port-type-t
+        (:type-none ,+type-none+)
+        (:type-send ,+type-send+)
+        (:type-receive ,+type-receive+)
+        (:type-send-once ,+type-send-once+)
+        (:type-port-set ,+type-port-set+)
+        (:type-dead-name ,+type-dead-name+)
+        (:type-send-receive ,+type-send-receive+)
+        (:type-send-rights ,+type-send-rights+)
+        (:type-port-rights ,+type-port-rights+)
+        (:type-port-or-dead ,+type-port-or-dead+)
+        (:type-all-rights ,+type-all-rights+)
+        (:type-dnrequest ,+type-dnrequest+)
+        (:type-compat ,+type-compat+)))))
 
 (%create-port-type-type)
 
