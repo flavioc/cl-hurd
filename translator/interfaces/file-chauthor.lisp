c99fadf3f71fe91dcb21dde9488a10d57df203c2
Flavio Cruz <flaviocruz@gmail.com>
2008-08-12 22:38:57 +0000

    Return :not-permitted instead of NIL.

--- translator/interfaces/file-chauthor.lisp
+++ translator/interfaces/file-chauthor.lisp
@@ -2,12 +2,14 @@
 (in-package :hurd-translator)
 
 (def-fs-interface :file-chauthor ((port port)
-								  (author uid-t))
+                                  (author uid-t))
   (with-lookup protid port
-    (when (allow-author-change-p *translator*
-                                 (get-node protid)
-                                 (get-user protid)
-                                 author)
-      (setf (stat-get (stat (get-node protid)) 'st-author) author))
-    t))
+    (cond
+      ((allow-author-change-p *translator*
+                              (get-node protid)
+                              (get-user protid)
+                              author)
+       (setf (stat-get (stat (get-node protid)) 'st-author) author)
+       t)
+      (t :not-permitted))))
 
