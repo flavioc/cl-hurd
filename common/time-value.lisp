e098c8a0740acc6d287803ff868415d5b0fea9df
Flavio Cruz <flaviocruz@gmail.com>
2008-08-13 17:06:18 +0000

    Make foreign pointer 'ptr' lazy.

--- common/time-value.lisp
+++ common/time-value.lisp
@@ -14,20 +14,26 @@ Definition can be found at mach/time_value.h"
   (microseconds :int))
 
 (defclass time-value ()
-  ((ptr :reader ptr
+  ((ptr :accessor ptr
+        :initform nil
         :initarg :ptr
-        :documentation "Pointer to a time-value struct."))
+        :documentation "Pointer to a time-value struct.")
+   (seconds :initarg :seconds
+            :accessor seconds
+            :initform nil
+            :documentation "Number of seconds.")
+   (microseconds :initarg :microseconds
+                 :accessor microseconds
+                 :initform nil
+                 :documentation "Number of miliseconds."))
   (:documentation "Time-value class for objects that allocate pointers to time-value structures."))
 
 (defun make-time-value (&key (seconds -1) (microseconds -1))
   "Create a new time-value object."
   (declare (type fixnum seconds microseconds))
-  (let ((ptr (foreign-alloc 'time-value-struct)))
-    (setf (foreign-slot-value ptr 'time-value-struct 'seconds) seconds
-          (foreign-slot-value ptr 'time-value-struct 'microseconds) microseconds)
-    (let ((obj (make-instance 'time-value :ptr ptr)))
-      (tg:finalize obj (lambda () (foreign-free ptr)))
-      obj)))
+  (make-instance 'time-value
+                 :seconds seconds
+                 :microseconds microseconds))
 
 (defconstant +now-time-value+ (make-time-value) "Current time value.")
 
@@ -42,26 +48,34 @@ Definition can be found at mach/time_value.h"
   (if (= -1 (foreign-slot-value value 'time-value-struct
                                 'microseconds))
     +now-time-value+
-    (make-instance 'time-value :ptr value)))
+    (make-instance 'time-value
+                   :seconds (foreign-slot-value value 'time-value-struct 'seconds)
+                   :microseconds (foreign-slot-value value 'time-value-struct 'microseconds))))
 
 (defmethod translate-to-foreign (value (type time-value-type))
   "Translate a time-value object to a foreign time-value pointer."
+  (unless (ptr value)
+    (let ((new-ptr (foreign-alloc 'time-value-struct)))
+      (setf (ptr value) new-ptr)
+      (tg:finalize value (lambda () (foreign-free new-ptr)))))
+  (setf (foreign-slot-value (ptr value)
+                            'time-value-struct 'seconds)
+        (seconds value)
+        (foreign-slot-value (ptr value)
+                            'time-value-struct 'microseconds)
+        (microseconds value))
   (ptr value))
 
 (defmethod time-value-seconds ((time time-value))
   "Returns the seconds value from a time-value 'time'."
-  (let ((ret (foreign-slot-value (ptr time)
-                                 'time-value-struct
-                                 'seconds)))
+  (let ((ret (seconds time)))
     (if (= -1 ret)
       (maptime-seconds *mapped-time*)
       ret)))
 
 (defmethod time-value-microseconds ((time time-value))
   "Returns the microseconds value from a time-value 'time'."
-  (let ((ret (foreign-slot-value (ptr time)
-                                 'time-value-struct
-                                 'microseconds)))
+  (let ((ret (microseconds time)))
     (if (= -1 ret)
       (maptime-microseconds *mapped-time*)
       ret)))
